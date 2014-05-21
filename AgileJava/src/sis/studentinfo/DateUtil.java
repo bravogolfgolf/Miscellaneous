c95c096e49eda870e1ad0aaebd563a98a9bedd6a
bravogolfgolf <bggibson@hotmail.com>
2014-05-21 14:50:25 -0400

    Updated DateUtilTest to test for a class method createDate(int, int, int) and then modified DateUtil.createDate(int, int, int) from instant to class method.
    Made constructor of DateUtil class private

--- AgileJava/src/sis/studentinfo/DateUtil.java
+++ AgileJava/src/sis/studentinfo/DateUtil.java
@@ -3,12 +3,13 @@ package sis.studentinfo;
 import java.util.*;
 
 public class DateUtil {
-   public Date createDate(int year, int month, int date) {
-      GregorianCalendar calendar = new GregorianCalendar();
-      calendar.clear();
-      calendar.set(Calendar.YEAR, year);
-      calendar.set(Calendar.MONTH, month - 1);
-      calendar.set(Calendar.DAY_OF_MONTH, date);
-      return calendar.getTime();
-   }
+	private DateUtil(){}
+	public static Date createDate(int year, int month, int date) {
+		GregorianCalendar calendar = new GregorianCalendar();
+		calendar.clear();
+		calendar.set(Calendar.YEAR, year);
+		calendar.set(Calendar.MONTH, month - 1);
+		calendar.set(Calendar.DAY_OF_MONTH, date);
+		return calendar.getTime();
+	}
 }
