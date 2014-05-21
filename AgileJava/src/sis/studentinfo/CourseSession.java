118a0b7b13e23deb68af9bce8a2dd02583db454b
bravogolfgolf <bggibson@hotmail.com>
2014-05-21 16:21:16 -0400

    Added CourseSession.count Class variable and incrementor code in constructor.

--- AgileJava/src/sis/studentinfo/CourseSession.java
+++ AgileJava/src/sis/studentinfo/CourseSession.java
@@ -8,62 +8,64 @@ import java.util.*;
  * @author Administrator
  */
 public class CourseSession {
-   private String department;
-   private String number;
-   private ArrayList<Student> students = new ArrayList<Student>();
-   private Date startDate;
+	static int count;
+	private String department;
+	private String number;
+	private ArrayList<Student> students = new ArrayList<Student>();
+	private Date startDate;
 
-   /**
-    * Constructs a CourseSession starting on a specific date
-    * @param startDate the date on which the CourseSession begins
-    */
-   public CourseSession(
-         String department, String number, Date startDate) {
-      this.department = department;
-      this.number = number;
-      this.startDate = startDate;
-   }
+	/**
+	 * Constructs a CourseSession starting on a specific date
+	 * @param startDate the date on which the CourseSession begins
+	 */
+	public CourseSession(
+			String department, String number, Date startDate) {
+		this.department = department;
+		this.number = number;
+		this.startDate = startDate;
+		CourseSession.count = CourseSession.count + 1;
+	}
 
-   String getDepartment() {
-      return department;
-   }
+	String getDepartment() {
+		return department;
+	}
 
-   String getNumber() {
-      return number;
-   }
+	String getNumber() {
+		return number;
+	}
 
-   int getNumberOfStudents() {
-      return students.size();
-   }
+	int getNumberOfStudents() {
+		return students.size();
+	}
 
-   public void enroll(Student student) {
-      students.add(student);
-   }
+	public void enroll(Student student) {
+		students.add(student);
+	}
 
-   Student get(int index) {
-      return students.get(index);
-   }
+	Student get(int index) {
+		return students.get(index);
+	}
 
-   Date getStartDate() {
-      return startDate;
-   }
+	Date getStartDate() {
+		return startDate;
+	}
 
-   public List<Student> getAllStudents() {
-      return students;
-   }
+	public List<Student> getAllStudents() {
+		return students;
+	}
 
-   /**
-    * @return Date the last date of the course session
-    */
-   Date getEndDate() {
-      GregorianCalendar calendar = new GregorianCalendar();
-      calendar.setTime(startDate);
-      final int sessionLength = 16;
-      final int daysInWeek = 7;
-      final int daysFromFridayToMonday = 3;
-      int numberOfDays =
-         sessionLength * daysInWeek - daysFromFridayToMonday;
-      calendar.add(Calendar.DAY_OF_YEAR, numberOfDays);
-      return calendar.getTime();
-   }
+	/**
+	 * @return Date the last date of the course session
+	 */
+	Date getEndDate() {
+		GregorianCalendar calendar = new GregorianCalendar();
+		calendar.setTime(startDate);
+		final int sessionLength = 16;
+		final int daysInWeek = 7;
+		final int daysFromFridayToMonday = 3;
+		int numberOfDays =
+				sessionLength * daysInWeek - daysFromFridayToMonday;
+		calendar.add(Calendar.DAY_OF_YEAR, numberOfDays);
+		return calendar.getTime();
+	}
 }
