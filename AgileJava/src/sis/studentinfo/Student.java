a23735cdc3e71c2a45f94a18db239877c5693bbc
bravogolfgolf <bggibson@hotmail.com>
2014-05-22 14:36:48 -0400

    added honorstudent test and code

--- AgileJava/src/sis/studentinfo/Student.java
+++ AgileJava/src/sis/studentinfo/Student.java
@@ -1,11 +1,18 @@
 package sis.studentinfo;
 
+import java.util.ArrayList;
+
 public class Student {
+	enum Grade {A, B, C, D, F}
 	static String IN_STATE = "CO";
-	static int CREDITS_REQUIRED_FOR_FULL_TIME = 12;
+	static final int CREDITS_REQUIRED_FOR_FULL_TIME = 12;
+	static final double HONOR_SCALER = 1;
 	private String name;
 	private int credits;
 	private String state = "";
+	private ArrayList<Student.Grade> grades = new ArrayList<Student.Grade>();
+	static final double  GRADE_A = 4.0, GRADE_B = 3.0, GRADE_C = 2.0, GRADE_D = 1.0;
+	private boolean isHonor = false;
 
 	public Student(String name) {
 		this.name = name;
@@ -29,5 +36,41 @@ public class Student {
 	}
 	boolean isInState(){
 		return state.equals(Student.IN_STATE);
+	}
+
+	double getGradePointAverage() {
+		if (grades.isEmpty()) {
+			return 0.0;
+		}
+		double numerator = 0.0;
+
+		if (getHonor() == false){
+			for(Grade grade : grades) {
+				if (grade == Student.Grade.A) numerator += GRADE_A;
+				else if (grade == Student.Grade.B) numerator += GRADE_B;
+				else if (grade == Student.Grade.C) numerator += GRADE_C;
+				else if (grade == Student.Grade.D) numerator += GRADE_D;
+			}
+		}
+		else if (getHonor() == true){
+			for(Grade grade : grades) {
+				if (grade == Student.Grade.A) numerator += GRADE_A + HONOR_SCALER;
+				else if (grade == Student.Grade.B) numerator += GRADE_B + HONOR_SCALER;
+				else if (grade == Student.Grade.C) numerator += GRADE_C + HONOR_SCALER;
+				else if (grade == Student.Grade.D) numerator += GRADE_D + HONOR_SCALER;
+			}
+		}
+		return numerator / grades.size();
+	}
+
+	void addGrade(Grade grade) {
+		grades.add(grade);
+	}
+
+	void setHonor() {
+		this.isHonor = true;
+	}
+	boolean getHonor(){
+		return this.isHonor;
 	}
 }
--- AgileJava/tst/sis/studentinfo/StudentTest.java
+++ AgileJava/tst/sis/studentinfo/StudentTest.java
@@ -3,6 +3,8 @@ package sis.studentinfo;
 import junit.framework.*;
 
 public class StudentTest extends TestCase {
+	private static final double GRADE_TOLERANCE = 0.05;
+
 	public void testCreate() {
 		final String firstStudentName = "First";
 		Student firstStudent = createStudent(firstStudentName);
@@ -16,7 +18,7 @@ public class StudentTest extends TestCase {
 		Student student  = createStudent("Status");
 		assertEquals(0,student.getCredits());
 		assertFalse(student.isFullTime());
-		
+
 		student.addCredits(3);
 		assertEquals(3,student.getCredits());
 		assertFalse(student.isFullTime());
@@ -24,7 +26,7 @@ public class StudentTest extends TestCase {
 		student.addCredits(4);
 		assertEquals(7,student.getCredits());
 		assertFalse(student.isFullTime());
-		
+
 		student.addCredits(5);
 		assertEquals(Student.CREDITS_REQUIRED_FOR_FULL_TIME,student.getCredits());
 		assertTrue(student.isFullTime());
@@ -36,6 +38,46 @@ public class StudentTest extends TestCase {
 		assertTrue(student.isInState());
 		student.setState("VA");
 		assertFalse(student.isInState());	
+	}
+	public void testCalculateGpa(){
+		Student student  = createStudent("TestGrade");
+		assertEquals(0.0,student.getGradePointAverage(),GRADE_TOLERANCE);
+		student.addGrade(Student.Grade.A);
+		assertEquals(4.0,student.getGradePointAverage(),GRADE_TOLERANCE);
+		student.addGrade(Student.Grade.B);
+		assertEquals(3.5,student.getGradePointAverage(),GRADE_TOLERANCE);
+		student.addGrade(Student.Grade.C);
+		assertEquals(3.0,student.getGradePointAverage(),GRADE_TOLERANCE);
+		student.addGrade(Student.Grade.D);
+		assertEquals(2.5,student.getGradePointAverage(),GRADE_TOLERANCE);
+		student.addGrade(Student.Grade.F);
+		assertEquals(2.0,student.getGradePointAverage(),GRADE_TOLERANCE);
+	}
+	public void testHonorStudent(){
+		Student honorStudent = createStudent("HonorStudent");
+		assertFalse(honorStudent.getHonor());
+		honorStudent.setHonor();
+		assertTrue(honorStudent.getHonor());
+	}
+	public void testCalculateHonorGpa(){
+		Student honorStudent  = createHonorStudent("TestHonorGrade");
+		assertEquals(0.0,honorStudent.getGradePointAverage(),GRADE_TOLERANCE);
+		honorStudent.addGrade(Student.Grade.A);
+		assertEquals(5.0,honorStudent.getGradePointAverage(),GRADE_TOLERANCE);
+		honorStudent.addGrade(Student.Grade.B);
+		assertEquals(4.5,honorStudent.getGradePointAverage(),GRADE_TOLERANCE);
+		honorStudent.addGrade(Student.Grade.C);
+		assertEquals(4.0,honorStudent.getGradePointAverage(),GRADE_TOLERANCE);
+		honorStudent.addGrade(Student.Grade.D);
+		assertEquals(3.5,honorStudent.getGradePointAverage(),GRADE_TOLERANCE);
+		honorStudent.addGrade(Student.Grade.F);
+		assertEquals(2.8,honorStudent.getGradePointAverage(),GRADE_TOLERANCE);
+	}
+
+	Student createHonorStudent(String name){
+		Student honorStudent = new Student(name);
+		honorStudent.setHonor();
+		return honorStudent;
 	}
 	Student createStudent(String name){
 		return new Student(name);
