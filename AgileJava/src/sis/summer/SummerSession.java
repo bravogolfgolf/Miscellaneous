d1b27774036c2941f3c60929954b05609bd18f3c
bravogolfgolf <bggibson@hotmail.com>
2014-05-23 11:58:32 -0400

    Created SummerSession and SummerSessionTest stubs

---
+++ AgileJava/src/sis/summer/SummerSession.java
@@ -0,0 +1,5 @@
+package sis.summer;
+
+class SummerSession {
+
+}
---
+++ AgileJava/tst/sis/summer/SummerSessionTest.java
@@ -0,0 +1,9 @@
+package sis.summer;
+
+import junit.framework.*;
+
+public class SummerSessionTest extends TestCase {
+	public void testCreate(){
+
+	}
+}
