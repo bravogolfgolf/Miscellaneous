65c8177d855a6fe2a7ff7bca6029c64949fba823
bravogolfgolf <bggibson@hotmail.com>
2014-05-22 22:36:39 -0400

    refactored RegularGradeStrategy to use switch

--- AgileJava/src/sis/studentinfo/RegularGradeStrategy.java
+++ AgileJava/src/sis/studentinfo/RegularGradeStrategy.java
@@ -3,10 +3,12 @@ package sis.studentinfo;
 public class RegularGradeStrategy implements GradeStrategy {
 	static final double  GRADE_A = 4.0, GRADE_B = 3.0, GRADE_C = 2.0, GRADE_D = 1.0;
 	public double getPointsForGrade(Student.Grade grade){
-		if (grade == Student.Grade.A) return RegularGradeStrategy.GRADE_A;
-		if (grade == Student.Grade.B) return RegularGradeStrategy.GRADE_B;
-		if (grade == Student.Grade.C) return RegularGradeStrategy.GRADE_C;
-		if (grade == Student.Grade.D) return RegularGradeStrategy.GRADE_D;
-		return 0.0;
+		switch (grade){
+		case A: return RegularGradeStrategy.GRADE_A;
+		case B: return RegularGradeStrategy.GRADE_B;
+		case C: return RegularGradeStrategy.GRADE_C;
+		case D: return RegularGradeStrategy.GRADE_D;
+		default: return 0.0;
+		}
 	}
 }
