f53313dd592925cf44844e9c12524a6e15fdd183
bravogolfgolf <bggibson@hotmail.com>
2014-06-10 23:18:25 -0400

    Correct minor error Changed SummerCourseSession to inherit from Session instead of CourseSession

--- AgileJava/src/sis/summer/SummerCourseSession.java
+++ AgileJava/src/sis/summer/SummerCourseSession.java
@@ -3,7 +3,7 @@ package sis.summer;
 import java.util.*;
 import sis.studentinfo.*;
 
-public class SummerCourseSession extends CourseSession{
+public class SummerCourseSession extends Session{
 	public static SummerCourseSession create(String department, String number, Date startDate) {
 		return new SummerCourseSession(department, number, startDate);
 	}
