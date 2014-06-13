01f37a5f26171add749f395d6639e97e94546b0f
bravogolfgolf <bggibson@hotmail.com>
2014-06-13 12:40:29 -0400

    Added and completed Account/Just before Logical Bit Operations section

--- AgileJava/tst/sis/studentinfo/PerformanceTest.java
+++ AgileJava/tst/sis/studentinfo/PerformanceTest.java
@@ -3,9 +3,14 @@ package sis.studentinfo;
 import junit.framework.TestCase;
 
 public class PerformanceTest extends TestCase {
+	private Performance performance;
 	private static final double TOLERANCE = 0.005;
+
+	protected void setUp(){
+		performance = new Performance();
+	}
+
 	public void testAverage(){
-		Performance performance = new Performance();
 		performance.setNumberOfTest(4);
 		performance.set(0,98);
 		performance.set(1,92);
@@ -14,5 +19,8 @@ public class PerformanceTest extends TestCase {
 
 		assertEquals(92.00, performance.get(1));
 		assertEquals(85.75,performance.average(), TOLERANCE);
+	}
+	public void testAverageForNoScore(){
+		assertEquals(0.0,performance.average(), TOLERANCE);
 	}
 }
