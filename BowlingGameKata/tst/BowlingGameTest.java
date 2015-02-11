a1d50651b0b694970c9fd2f0c3e920737aafeeb6
bravogolfgolf <bggibson@hotmail.com>
2015-02-11 14:40:05 -0500

    Refactor duplicate Game creation

--- BowlingGameKata/tst/BowlingGameTest.java
+++ BowlingGameKata/tst/BowlingGameTest.java
@@ -1,14 +1,19 @@
 import junit.framework.TestCase;
 
 public class BowlingGameTest extends TestCase {
+	private Game g;
+
+	protected void setUp(){
+		g = new Game();
+	}
+
 	public void testGutterGame() throws Exception {
-		Game g = new Game();
 		for(int i=0;i<20;i++)
 			g.roll(0);
 		assertEquals(0,g.score());
 	}
+
 	public void testAllOnes() throws Exception {
-		Game g = new Game();
 		for(int i=0;i<20;i++)
 			g.roll(1);
 		assertEquals(20,g.score());
