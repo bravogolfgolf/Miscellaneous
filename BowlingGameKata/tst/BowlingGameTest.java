c3cbf583d9d0c247e5feaf822e5d98cf7bc38f0f
bravogolfgolf <bggibson@hotmail.com>
2015-02-11 14:15:28 -0500

    Failing test after adding g.roll(0)

--- BowlingGameKata/tst/BowlingGameTest.java
+++ BowlingGameKata/tst/BowlingGameTest.java
@@ -3,5 +3,7 @@ import junit.framework.TestCase;
 public class BowlingGameTest extends TestCase {
 	public void testGutterGame() throws Exception {
 		Game g = new Game();
+		for(int i=0;i<20;i++)
+			g.roll(0);
 	}
 }
