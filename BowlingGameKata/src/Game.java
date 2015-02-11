66f38d30f801edfa83c67a52cbe8fe664dbc3860
bravogolfgolf <bggibson@hotmail.com>
2015-02-11 15:44:22 -0500

    Refactored Game methods roll() and score(). Added array to roll() and it now keeps track of pins rolled and does not calculate score. Added for loop to score() and now calculates score using array. First two test still pass.

--- BowlingGameKata/src/Game.java
+++ BowlingGameKata/src/Game.java
@@ -1,11 +1,15 @@
 public class Game {
 	private int score = 0;
+	private int rolls[] = new int[21];
+	private int currentRoll = 0;
 
 	public void roll(int pins){
-		score = score + pins;
+		rolls[currentRoll = currentRoll +1] = pins;
 	}
 
 	public int score() {
+		for (int i = 0; i < rolls.length; i++)
+			score = score + rolls[i];
 		return score;
 	}
 }
