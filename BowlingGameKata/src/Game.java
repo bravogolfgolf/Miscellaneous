94e9a29f4a4e4cbd98a884ae25c1e6af483cea16
bravogolfgolf <bggibson@hotmail.com>
2015-02-15 15:14:38 -0500

    Refactored to include isSpare() instead of difficult conditional statement

--- BowlingGameKata/src/Game.java
+++ BowlingGameKata/src/Game.java
@@ -8,16 +8,20 @@ public class Game {
 
 	public int score() {
 		int score = 0;
-		int i = 0;
+		int frameIndex = 0;
 		for (int frame = 0; frame < 10; frame++)  {
-			if (rolls[i] + rolls[i+1] == 10) {
-				score += rolls[i] + rolls[i+1] + rolls[i+2];
+			if (isSpare(frameIndex)) {
+				score += rolls[frameIndex] + rolls[frameIndex+1] + rolls[frameIndex+2];
 
 			}else{
-				score += rolls[i] + rolls[i+1];	
+				score += rolls[frameIndex] + rolls[frameIndex+1];	
 			}
-			i += 2;
+			frameIndex += 2;
 		}
 		return score;
+	}
+
+	private boolean isSpare(int frameIndex) {
+		return rolls[frameIndex] + rolls[frameIndex+1] == 10;
 	}
 }
