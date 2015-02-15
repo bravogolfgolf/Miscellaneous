d5a54d058b965ce7b8e7ae7444e276297ec6878c
bravogolfgolf <bggibson@hotmail.com>
2015-02-15 15:24:13 -0500

    Added Strike test and code...all tests pass

--- BowlingGameKata/src/Game.java
+++ BowlingGameKata/src/Game.java
@@ -10,13 +10,17 @@ public class Game {
 		int score = 0;
 		int frameIndex = 0;
 		for (int frame = 0; frame < 10; frame++)  {
-			if (isSpare(frameIndex)) {
+			if(rolls[frameIndex] == 10){
 				score += rolls[frameIndex] + rolls[frameIndex+1] + rolls[frameIndex+2];
-
+				frameIndex += 1;
+			}
+			else if (isSpare(frameIndex)) {
+				score += rolls[frameIndex] + rolls[frameIndex+1] + rolls[frameIndex+2];
+				frameIndex += 2;
 			}else{
 				score += rolls[frameIndex] + rolls[frameIndex+1];	
+				frameIndex += 2;
 			}
-			frameIndex += 2;
 		}
 		return score;
 	}
--- BowlingGameKata/tst/BowlingGameTest.java
+++ BowlingGameKata/tst/BowlingGameTest.java
@@ -35,6 +35,14 @@ public class BowlingGameTest extends TestCase {
 		assertEquals(16,g.score());
 	}
 
+	public void testOneStrike() throws Exception {
+	    g.roll(10); // strike
+	    g.roll(3);
+	    g.roll(4);
+	    rollMany(16, 0);
+	    assertEquals(24, g.score());
+	  }
+
 	private void rollSpare() {
 		g.roll(5);
 		g.roll(5);
