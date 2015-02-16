5e4f5f7782c2cc26acfebac918eede2cffb2ef04
bravogolfgolf <bggibson@hotmail.com>
2015-02-16 18:20:51 -0500

    All Test Pass

--- PrimeFactors/src/PrimeFactors.java
+++ PrimeFactors/src/PrimeFactors.java
@@ -5,11 +5,12 @@ public class PrimeFactors {
 	public ArrayList<Integer> find(int numberToFactor ) {
 		ArrayList<Integer> factors = new ArrayList<Integer>();
 		int candidate = 2;
-		if (numberToFactor > 1){
+		while (numberToFactor > 1){
 			while (numberToFactor % candidate == 0){
 				factors.add(candidate);
 				numberToFactor /= candidate;
 			}
+			candidate++;
 		}
 		if (numberToFactor > 1)
 			factors.add(numberToFactor);
