5c777fe80e179755f3972e9c0d2a06fb26b6d43f
bravogolfgolf <bggibson@hotmail.com>
2015-02-16 12:51:00 -0500

    Second test passes

--- PrimeFactors/src/PrimeFactors.java
+++ PrimeFactors/src/PrimeFactors.java
@@ -4,6 +4,8 @@ public class PrimeFactors {
 
 	public ArrayList<Integer> find(int howManyPrimeFactors) {
 		ArrayList<Integer> primeFactors = new ArrayList<Integer>();
+		if (howManyPrimeFactors > 1)
+			primeFactors.add(2);
 		return primeFactors;
 	}
 }
--- PrimeFactors/tst/PrimeFactorsTest.java
+++ PrimeFactors/tst/PrimeFactorsTest.java
@@ -7,4 +7,10 @@ public class PrimeFactorsTest extends TestCase {
 		ArrayList<Integer> knownList = new ArrayList<Integer>();
 		assertEquals(knownList, primefactors.find(1));
 	}
+	public void testTwo(){
+		PrimeFactors primefactors = new PrimeFactors();
+		ArrayList<Integer> knownList = new ArrayList<Integer>();
+		knownList.add(2);
+		assertEquals(knownList, primefactors.find(2));
+	}
 }
