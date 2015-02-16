76308c286d7d89d778d32d37adad0bd5c2fad6b9
bravogolfgolf <bggibson@hotmail.com>
2015-02-16 18:35:36 -0500

    refactored outer while loop

--- PrimeFactors/src/PrimeFactors.java
+++ PrimeFactors/src/PrimeFactors.java
@@ -4,13 +4,11 @@ public class PrimeFactors {
 
 	public ArrayList<Integer> find(int numberToFactor ) {
 		ArrayList<Integer> factors = new ArrayList<Integer>();
-		int candidate = 2;
-		while (numberToFactor > 1){
+		
+		for (int candidate = 2; numberToFactor > 1; candidate++){
 			for (;numberToFactor % candidate == 0; numberToFactor /= candidate){
-				factors.add(candidate);
-				
+				factors.add(candidate);	
 			}
-			candidate++;
 		}
 		return factors;
 	}
