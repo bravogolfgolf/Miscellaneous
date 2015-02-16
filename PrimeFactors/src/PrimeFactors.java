a316c13b42c301637a517296d70aafb1e2c48f75
bravogolfgolf <bggibson@hotmail.com>
2015-02-16 16:48:09 -0500

    TestThree passes

--- PrimeFactors/src/PrimeFactors.java
+++ PrimeFactors/src/PrimeFactors.java
@@ -5,7 +5,7 @@ public class PrimeFactors {
 	public ArrayList<Integer> find(int numberToFactor ) {
 		ArrayList<Integer> factors = new ArrayList<Integer>();
 		if (numberToFactor > 1)
-			factors.add(2);
+			factors.add(numberToFactor);
 		return factors;
 	}
 
