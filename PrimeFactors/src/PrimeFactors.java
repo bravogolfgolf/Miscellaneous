84d52eda27f540d24426e0944271ff7c85ab11d4
bravogolfgolf <bggibson@hotmail.com>
2015-02-16 12:42:09 -0500

    Removed add(1) from test and code...i guess requirments are to return nothing if input is 1 judging from second test I looked at.

--- PrimeFactors/src/PrimeFactors.java
+++ PrimeFactors/src/PrimeFactors.java
@@ -4,7 +4,6 @@ public class PrimeFactors {
 
 	public ArrayList<Integer> find(int howManyPrimeFactors) {
 		ArrayList<Integer> primeFactors = new ArrayList<Integer>();
-		primeFactors.add(1);
 		return primeFactors;
 	}
 }
