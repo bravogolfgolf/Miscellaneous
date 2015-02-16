e635352e0378e0cc1e4e752e06b654e8ee6646b1
bravogolfgolf <bggibson@hotmail.com>
2015-02-16 17:19:08 -0500

    All Tests Pass

--- PrimeFactors/src/PrimeFactors.java
+++ PrimeFactors/src/PrimeFactors.java
@@ -4,9 +4,14 @@ public class PrimeFactors {
 
 	public ArrayList<Integer> find(int numberToFactor ) {
 		ArrayList<Integer> factors = new ArrayList<Integer>();
+		if (numberToFactor > 1){
+			if (numberToFactor % 2 == 0){
+				factors.add(2);
+				numberToFactor /= 2;
+			}
+		}
 		if (numberToFactor > 1)
 			factors.add(numberToFactor);
 		return factors;
 	}
-
 }
--- PrimeFactors/tst/PrimeFactorsTest.java
+++ PrimeFactors/tst/PrimeFactorsTest.java
@@ -3,24 +3,31 @@ import junit.framework.TestCase;
 
 
 public class PrimeFactorsTest extends TestCase{
-	
+
 	private PrimeFactors pm = new PrimeFactors();
 	private ArrayList<Integer> list = new ArrayList<Integer>();
-	
+
 	public void testOne() throws Exception {
-		assertEquals(list,pm.find(1));
+		assertEquals(list, pm.find(1));
 	}
 	public void testTwo() throws Exception {
 		createList(2);
-		assertEquals(list,pm.find(2));
+		assertEquals(list, pm.find(2));
 	}
 	public void testThree() throws Exception {
 		createList(3);
-		assertEquals(list,pm.find(3));
+		assertEquals(list, pm.find(3));
 	}
-	private void createList(int i) {
+
+	public void testFour() throws Exception {
+		createList(2,2);
+		assertEquals(list, pm.find(4));
+	}
+
+	private void createList(int...ints) {
+		for(int i : ints)
 		list.add(i);
 	}
-	
+
 
 }
