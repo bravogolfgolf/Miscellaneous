f066b010c038f6edbde6f72697d5b84e37936a6a
bravogolfgolf <bggibson@hotmail.com>
2015-02-17 12:13:29 -0500

    All test pass

--- ConvertRomanAndArabicNumbers/src/com/convert/www/Arabic.java
+++ ConvertRomanAndArabicNumbers/src/com/convert/www/Arabic.java
@@ -3,52 +3,74 @@ package com.convert.www;
 public class Arabic {
 	StringBuffer symbols = new StringBuffer();
 	StringBuffer numbersRepresentingSymbols = new StringBuffer();
+	int[] integersRepresentingSymbols;
 
 	public String toRoman(int arabicNumber) {
-		appendProperSymbols(arabicNumber);
-		System.out.println(numbersRepresentingSymbols.toString());
+		int howManyTens = 0;
+		int howManyOnes = 0;
+
+		howManyTens = Math.round((arabicNumber / 10) - 0.5f);
+		howManyOnes = arabicNumber - (howManyTens * 10);
+
+		appendProperSymbols(howManyTens, 10);
+		appendProperSymbols(howManyOnes, 1);
+
+
+		String commaDelimited = new String(numbersRepresentingSymbols.toString());
+		String[] parts = commaDelimited.split(",");
+		for(int n = 0; n < parts.length; n++) {
+			switch (Integer.parseInt(parts[n])) {
+			case 1: symbols.append("I"); break;
+			case 5: symbols.append("V"); break;
+			case 10: symbols.append("X"); break;
+			}	   
+		}
 		return symbols.toString();
 	}
 
-	private void appendProperSymbols(int number) {
+	private void appendProperSymbols(int number, int multiplier) {
+
 		if (number == 9) {
-			appendOne(1);
-			appendTen();
+			appendOne(1,multiplier);
+			appendTen(multiplier);
 		}
 
 		else if (number > 5) {
 			int numberOfOnesToAppend = number % 5;
-			appendFive();
-			appendOne(numberOfOnesToAppend);
+			appendFive(multiplier);
+			appendOne(numberOfOnesToAppend,multiplier);
 		}
 
 		else if (number == 5) {
-			appendFive();
+			appendFive(multiplier);
 		}
 
 		else if (number == 4) {
-			appendOne(1);
-			appendFive();
+			appendOne(1,multiplier);
+			appendFive(multiplier);
 
 		}
-		else { appendOne(number);
+		else { appendOne(number,multiplier);
 		}
 	}
 
-	private void appendTen() {
-		numbersRepresentingSymbols.append("10,");
-		symbols.append("X");
+	private void appendTen(int multiplier) {
+		Integer numbersRepresentingSymbol = 10 * multiplier;
+		numbersRepresentingSymbols.append(numbersRepresentingSymbol.toString() + ",");
+
 	}
 
-	private void appendFive() {
-		numbersRepresentingSymbols.append("5,");
-		symbols.append("V");
+	private void appendFive(int multiplier) {
+		Integer numbersRepresentingSymbol = 5 * multiplier;
+		numbersRepresentingSymbols.append(numbersRepresentingSymbol.toString() + ",");
+	
 	}
 
-	private void appendOne(int numberOfOnesToAppend) {
+	private void appendOne(int numberOfOnesToAppend, int multiplier) {
 		for(int i = 1; i <= numberOfOnesToAppend; i++){
-			numbersRepresentingSymbols.append("1,");
-			symbols.append("I");
+			Integer numbersRepresentingSymbol = 1 * multiplier;
+			numbersRepresentingSymbols.append(numbersRepresentingSymbol.toString() + ",");
+		
 		}
 	}
 }
