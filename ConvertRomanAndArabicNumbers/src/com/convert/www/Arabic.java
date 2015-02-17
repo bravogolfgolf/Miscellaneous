a54103aca32f0dd727711f2ddce10fc3cb1387aa
bravogolfgolf <bggibson@hotmail.com>
2015-02-17 10:34:46 -0500

    refactoring

--- ConvertRomanAndArabicNumbers/src/com/convert/www/Arabic.java
+++ ConvertRomanAndArabicNumbers/src/com/convert/www/Arabic.java
@@ -1,36 +1,54 @@
 package com.convert.www;
 
 public class Arabic {
-	StringBuffer sb = new StringBuffer();
-	
-	public String toRoman(int arabicNumber) {
-		
-		if (arabicNumber == 9) {
-			sb.append("I");
-			sb.append("X");
-		}
-		
-		else if (arabicNumber > 5) {
-			sb.append("V");
-			appendOnes(arabicNumber % 5);
-		}
-		
-		else if (arabicNumber == 5) {
-			sb.append("V");
-		}
-		
-		else if (arabicNumber == 4) {
-			sb.append("I");
-			sb.append("V");
+	StringBuffer symbols = new StringBuffer();
+	StringBuffer numbersRepresentingSymbols = new StringBuffer();
 
-		}
-		else { appendOnes(arabicNumber);
-		}
-		return sb.toString();
+	public String toRoman(int arabicNumber) {
+		appendProperSymbols(arabicNumber);
+		System.out.println(numbersRepresentingSymbols.toString());
+		return symbols.toString();
 	}
 
-	private void appendOnes(int numberOfOnesToAppend) {
-		for(int i = 1; i <= numberOfOnesToAppend; i++)
-			sb.append("I");
+	private void appendProperSymbols(int number) {
+		if (number == 9) {
+			appendOne(1);
+			appendTen();
+		}
+
+		else if (number > 5) {
+			int numberOfOnesToAppend = number % 5;
+			appendFive();
+			appendOne(numberOfOnesToAppend);
+		}
+
+		else if (number == 5) {
+			appendFive();
+		}
+
+		else if (number == 4) {
+			appendOne(1);
+			appendFive();
+
+		}
+		else { appendOne(number);
+		}
+	}
+
+	private void appendTen() {
+		numbersRepresentingSymbols.append("10,");
+		symbols.append("X");
+	}
+
+	private void appendFive() {
+		numbersRepresentingSymbols.append("5,");
+		symbols.append("V");
+	}
+
+	private void appendOne(int numberOfOnesToAppend) {
+		for(int i = 1; i <= numberOfOnesToAppend; i++){
+			numbersRepresentingSymbols.append("1,");
+			symbols.append("I");
+		}
 	}
 }
