a3db60a1e764ee323546a36b7d9ecba664c1052f
bravogolfgolf <bggibson@hotmail.com>
2015-02-16 22:11:29 -0500

    Test testOnetoI passes

---
+++ ConvertRomanAndArabicNumbers/src/com/convert/www/Arabic.java
@@ -0,0 +1,9 @@
+package com.convert.www;
+
+public class Arabic {
+
+	public static String toRoman(int i) {
+		return "I";
+	}
+
+}
--- ConvertRomanAndArabicNumbers/tst/com/convert/www/ConvertArabicToRomanTest.java
+++ ConvertRomanAndArabicNumbers/tst/com/convert/www/ConvertArabicToRomanTest.java
@@ -1,5 +1,6 @@
 package com.convert.www;
 import org.junit.Test;
+
 import junit.framework.TestCase;
 
 public class ConvertArabicToRomanTest extends TestCase {
