53d2c0a6efd9d500a75b95c947a4aa860dcafe9e
bravogolfgolf <bggibson@hotmail.com>
2015-02-16 22:47:13 -0500

    All tests pass

--- ConvertRomanAndArabicNumbers/src/com/convert/www/Arabic.java
+++ ConvertRomanAndArabicNumbers/src/com/convert/www/Arabic.java
@@ -2,8 +2,10 @@ package com.convert.www;
 
 public class Arabic {
 
-	public static String toRoman(int i) {
-		return "I";
+	public static String toRoman(int arabicNumber) {
+		StringBuffer sb = new StringBuffer();
+		for(int i = 1; i <= arabicNumber; i++)
+			sb.append("I");
+		return sb.toString();
 	}
-
 }
