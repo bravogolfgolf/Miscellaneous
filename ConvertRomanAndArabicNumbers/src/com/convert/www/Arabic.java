9293a3bc3a2454fd7caf08f45e162413d256f9e8
bravogolfgolf <bggibson@hotmail.com>
2015-02-16 23:19:46 -0500

    All test pass

--- ConvertRomanAndArabicNumbers/src/com/convert/www/Arabic.java
+++ ConvertRomanAndArabicNumbers/src/com/convert/www/Arabic.java
@@ -5,13 +5,15 @@ public class Arabic {
 	public static String toRoman(int arabicNumber) {
 		StringBuffer sb = new StringBuffer();
 
-		if (arabicNumber == 4) {
+		if (arabicNumber == 5) {
+			sb.append("V");
+		}
+		else if (arabicNumber == 4) {
 			sb.append("IV");
-			
-		} else {
 
-			for(int i = 1; i <= arabicNumber; i++)
-				sb.append("I");
+		}
+		else { for(int i = 1; i <= arabicNumber; i++)
+			sb.append("I");
 		}
 		return sb.toString();
 	}
