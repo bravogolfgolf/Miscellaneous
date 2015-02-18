fa4e0c3fd2a435c4e9bf7d6235f27ea9be434f22
bravogolfgolf <bggibson@hotmail.com>
2015-02-18 16:10:22 -0500

    First test

--- Sort/src/TestSort.java
+++ Sort/src/TestSort.java
@@ -1,13 +1,24 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.Test;
 
 
 public class TestSort {
 
+	private List<Integer> list(Integer...ints) {return Arrays.asList(ints);}
+
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testSortArrays() {
+		assertEquals(list(),sort());
 	}
+
+	private List<Integer> sort() {
+		return new ArrayList<Integer>();
+	}
+
 
 }
