86a35c90c6e0866043013e461f4d7c374c3232c4
bravogolfgolf <bggibson@hotmail.com>
2015-02-18 13:29:36 -0500

    test 8 passes

--- PF/tst/PrimeFactorsTest.java
+++ PF/tst/PrimeFactorsTest.java
@@ -22,7 +22,7 @@ public class PrimeFactorsTest {
 	private List<Integer> determinePrimeFactors(int i) {
 		List<Integer> factors = new ArrayList<Integer>();
 		if(i > 1){
-			if (i%2==0){
+			while (i%2==0){
 				factors.add(2);
 				i/=2;
 			}
