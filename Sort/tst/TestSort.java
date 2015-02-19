18bee14ec56884465b92078ef49f3ce0d5af37e3
bravogolfgolf <bggibson@hotmail.com>
2015-02-18 21:51:41 -0500

    Moved sort code to new Class

--- Sort/tst/TestSort.java
+++ Sort/tst/TestSort.java
@@ -1,10 +1,5 @@
-
 import static org.junit.Assert.*;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
+import java.util.*;
 import org.junit.Test;
 
 
@@ -14,32 +9,16 @@ public class TestSort {
 
 	@Test
 	public void testSortArrays() {
-		assertEquals(list(),sort());
-		assertEquals(list(1),sort(1));
-		assertEquals(list(1,2),sort(1,2));
-		assertEquals(list(1,2),sort(2,1));
-		assertEquals(list(1,2,3),sort(1,2,3));
-		assertEquals(list(1,2,3),sort(2,1,3));
-		assertEquals(list(1,2,3),sort(2,3,1));
-	}
 
-	private List<Integer> sort(Integer...ints) {
-		ArrayList<Integer> result = new ArrayList<Integer>();
-		if (ints.length > 0){
-			for (Integer j : ints){
-				result.add(j);
-			}
-			if (result.size() > 1)
-				for (int i = 0; i < result.size() - 1; i++) {
-					if(result.get(i) > result.get(i+1)){
-						Integer high = result.get(i+1);
-						Integer low = result.get(i);						
-						result.set(i, high);
-						result.set(i+1, low);
-					}
-					i = 0;
-				}
-		}
-		return result;
+		Sorter s = new Sorter();
+		
+		assertEquals(list(),s.sort());
+		assertEquals(list(1),s.sort(1));
+		assertEquals(list(1,2),s.sort(1,2));
+		assertEquals(list(1,2),s.sort(2,1));
+		assertEquals(list(1,2,3),s.sort(1,2,3));
+		assertEquals(list(1,2,3),s.sort(2,1,3));
+		assertEquals(list(1,2,3),s.sort(2,3,1));
 	}
 }
+	
