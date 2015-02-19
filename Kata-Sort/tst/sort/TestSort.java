967fe6ddc94582514ad811edf4408f16957a4567
bravogolfgolf <bggibson@hotmail.com>
2015-02-19 14:24:29 -0500

    All tests pass...Complete

--- Kata-Sort/tst/sort/TestSort.java
+++ Kata-Sort/tst/sort/TestSort.java
@@ -12,12 +12,28 @@ public class TestSort {
 	public void testSortArrays() {		
 		assertEquals(list(), Sorter.sort());
 		assertEquals(list(1), Sorter.sort(1));
+		
 		assertEquals(list(1,1), Sorter.sort(1,1));
 		assertEquals(list(1,2), Sorter.sort(1,2));
 		assertEquals(list(1,2), Sorter.sort(2,1));
+		
 		assertEquals(list(1,2,2), Sorter.sort(1,2,2));
 		assertEquals(list(1,2,2), Sorter.sort(2,1,2));
 		assertEquals(list(1,2,2), Sorter.sort(2,2,1));
+		
+		assertEquals(list(1,2,3), Sorter.sort(1,2,3));
+		assertEquals(list(1,2,3), Sorter.sort(1,3,2));
+		assertEquals(list(1,2,3), Sorter.sort(2,1,3));
+		assertEquals(list(1,2,3), Sorter.sort(2,3,1));
+		assertEquals(list(1,2,3), Sorter.sort(3,1,2));
+		assertEquals(list(1,2,3), Sorter.sort(3,2,1));
+		
+		assertEquals(list(1,2,3,4), Sorter.sort(1,2,3,4));
+		assertEquals(list(1,2,3,4), Sorter.sort(4,1,2,3));
+		assertEquals(list(1,2,3,4), Sorter.sort(3,4,1,2));
+		assertEquals(list(1,2,3,4), Sorter.sort(2,3,4,1));
+		
+		assertEquals(list(1,2,3,2147483647), Sorter.sort(2147483647,1,2,3));
 	}
 }
 	
