435418038f85a1d9bd41614d65d7ae9b73c8323a
bravogolfgolf <bggibson@hotmail.com>
2015-02-19 11:16:23 -0500

    Test sort unordered pair passes

--- Kata-Sort/tst/sort/Sorter.java
+++ Kata-Sort/tst/sort/Sorter.java
@@ -4,8 +4,16 @@ import java.util.*;
 
 public class Sorter {
 
-	public static List<Integer> sort() {
-		return new ArrayList<Integer>();
+	public static List<Integer> sort(Integer...ints) {
+		List<Integer> asList = Arrays.asList(ints);
+		if(asList.size() > 1){
+			if(asList.get(0) > asList.get(1)){
+				Integer temp = asList.get(1);
+				asList.set(1, asList.get(0));
+				asList.set(0, temp);
+			}
+		}
+		return asList;
 	}
 
 }
--- Kata-Sort/tst/sort/TestSort.java
+++ Kata-Sort/tst/sort/TestSort.java
@@ -6,11 +6,15 @@ import org.junit.Test;
 
 public class TestSort {
 
-	private List<Integer> list() {return Arrays.asList();}
+	private List<Integer> list(Integer...ints) {return Arrays.asList(ints);}
 
 	@Test
 	public void testSortArrays() {		
 		assertEquals(list(), Sorter.sort());
+		assertEquals(list(1), Sorter.sort(1));
+		assertEquals(list(1,1), Sorter.sort(1,1));
+		assertEquals(list(1,2), Sorter.sort(1,2));
+		assertEquals(list(1,2), Sorter.sort(2,1));
 	}
 }
 	
