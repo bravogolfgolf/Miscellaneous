e793ecbd270a53673acf44dfe29840c62766f454
bravogolfgolf <bggibson@hotmail.com>
2015-02-19 15:29:42 -0500

    Test 5,6,7 worked...Test 8 passes

--- Kata-PrimeFactors/src/primeFactors/PrimeFactors.java
+++ Kata-PrimeFactors/src/primeFactors/PrimeFactors.java
@@ -6,7 +6,7 @@ public class PrimeFactors {
 
 	public static List<Integer> find(int i) {
 		ArrayList<Integer> arrayList = new ArrayList<Integer>();
-		if (i % 2 == 0){
+		while (i % 2 == 0){
 			arrayList.add(2);
 			i /= 2;
 		}
--- Kata-PrimeFactors/tst/primeFactors/PrimeFacrorsTest.java
+++ Kata-PrimeFactors/tst/primeFactors/PrimeFacrorsTest.java
@@ -15,11 +15,11 @@ public class PrimeFacrorsTest {
 		assertEquals(list(2), find(2));
 		assertEquals(list(3), find(3));
 		assertEquals(list(2,2), find(4));
-/*		assertEquals(list(5), find(5));
+		assertEquals(list(5), find(5));
 		assertEquals(list(2,3), find(6));
 		assertEquals(list(7), find(7));
 		assertEquals(list(2,2,2), find(8));
-		assertEquals(list(3,3), find(9));
+/*		assertEquals(list(3,3), find(9));
 		assertEquals(list(3,3,3), find(27));
 		assertEquals(list(2,3,3,7,11,31,151,331), find(2147483646));
 		assertEquals(list(2147483647), find(2147483647));
