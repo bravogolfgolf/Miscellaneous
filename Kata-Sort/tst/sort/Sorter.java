e633c248c9a535583e5b585f45ab3b198f3ca383
bravogolfgolf <bggibson@hotmail.com>
2015-02-19 10:49:53 -0500

    First test passes

--- Kata-Sort/tst/sort/Sorter.java
+++ Kata-Sort/tst/sort/Sorter.java
@@ -1,35 +1,11 @@
 package sort;
-import java.util.ArrayList;
-import java.util.List;
+
+import java.util.*;
 
 public class Sorter {
 
-	private ArrayList<Integer> result = new ArrayList<Integer>();
-
-	public List<Integer> sort(Integer...ints) {
-		if (ints.length > 0){
-			createInputArray(ints);
-			sortArray();
-		}
-		return result;
+	public static Object sort() {
+		return new ArrayList();
 	}
 
-	private void sortArray() {
-		if (result.size() > 1)
-			for (int i = 0; i < result.size() - 1; i++) {
-				if(result.get(i) > result.get(i+1)){
-					Integer high = result.get(i+1);
-					Integer low = result.get(i);						
-					result.set(i, high);
-					result.set(i+1, low);
-				}
-				i = 0;
-			}
-	}
-
-	private void createInputArray(Integer... ints) {
-		for (Integer j : ints){
-			result.add(j);
-		}
-	}
 }
--- Kata-Sort/tst/sort/TestSort.java
+++ Kata-Sort/tst/sort/TestSort.java
@@ -1,27 +1,16 @@
 package sort;
 import static org.junit.Assert.*;
-
 import java.util.*;
-
 import org.junit.Test;
 
 
 public class TestSort {
 
-	private List<Integer> list(Integer...ints) {return Arrays.asList(ints);}
+	private List<Integer> list() {return Arrays.asList();}
 
 	@Test
-	public void testSortArrays() {
-
-		Sorter s = new Sorter();
-		
-		assertEquals(list(),s.sort());
-		assertEquals(list(1),s.sort(1));
-		assertEquals(list(1,2),s.sort(1,2));
-		assertEquals(list(1,2),s.sort(2,1));
-		assertEquals(list(1,2,3),s.sort(1,2,3));
-		assertEquals(list(1,2,3),s.sort(2,1,3));
-		assertEquals(list(1,2,3),s.sort(2,3,1));
+	public void testSortArrays() {		
+		assertEquals(list(), Sorter.sort());
 	}
 }
 	
