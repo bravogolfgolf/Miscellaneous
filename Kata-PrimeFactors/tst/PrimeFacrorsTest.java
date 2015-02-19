1c21dfe28c06ebce8a42e44ab74a3e49b3b21d3d
bravogolfgolf <bggibson@hotmail.com>
2015-02-19 06:21:17 -0500

    Second test passes

--- Kata-PrimeFactors/tst/PrimeFacrorsTest.java
+++ Kata-PrimeFactors/tst/PrimeFacrorsTest.java
@@ -2,16 +2,15 @@ import static org.junit.Assert.*;
 import java.util.*;
 import org.junit.Test;
 
-
 public class PrimeFacrorsTest {
 
 	@Test
 	public void testPrimeFactors() {
-		assertEquals(list(),PrimeFactors.find());
+		assertEquals(list(), PrimeFactors.find(1));
+		assertEquals(list(2), PrimeFactors.find(2));
 	}
 
-	private List<Integer> list() {
-		return new ArrayList<Integer>();
+	private List<Integer> list(Integer...ints) {
+		return Arrays.asList(ints);
 	}
-
 }
