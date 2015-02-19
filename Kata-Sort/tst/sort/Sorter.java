c6fdb57f2237ee16b18a9012857e9d88605ceccf
bravogolfgolf <bggibson@hotmail.com>
2015-02-19 10:51:13 -0500

    Refactored Sorter.sort()

--- Kata-Sort/tst/sort/Sorter.java
+++ Kata-Sort/tst/sort/Sorter.java
@@ -4,8 +4,8 @@ import java.util.*;
 
 public class Sorter {
 
-	public static Object sort() {
-		return new ArrayList();
+	public static List<Integer> sort() {
+		return new ArrayList<Integer>();
 	}
 
 }
