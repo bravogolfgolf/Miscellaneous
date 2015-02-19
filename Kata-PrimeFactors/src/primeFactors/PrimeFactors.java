4f123114ef9bc443376ea2b949c0f9270f1c3b32
bravogolfgolf <bggibson@hotmail.com>
2015-02-19 16:00:57 -0500

    All tests pass...Complete.

--- Kata-PrimeFactors/src/primeFactors/PrimeFactors.java
+++ Kata-PrimeFactors/src/primeFactors/PrimeFactors.java
@@ -7,12 +7,9 @@ public class PrimeFactors {
 	public static List<Integer> find(int i) {
 		ArrayList<Integer> arrayList = new ArrayList<Integer>();
 
-		for(int divisor = 2; i > 1; divisor++) {
-			while (i % divisor == 0){
+		for(int divisor = 2; i > 1; divisor++)
+			for (;i % divisor == 0; i /= divisor)
 				arrayList.add(divisor);
-				i /= divisor;
-			}	
-		}
 		return arrayList;
 	}
 }
