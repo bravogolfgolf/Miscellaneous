b741ac5d3e3ae9dd50a0e8753aaea73f0a442df5
bravogolfgolf <bggibson@hotmail.com>
2015-02-20 11:47:15 -0500

    Created Project with Test Class

---
+++ Kata-StringCalculator/tst/stringCalculator/TestStringCalculator.java
@@ -0,0 +1,14 @@
+package stringCalculator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestStringCalculator {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
