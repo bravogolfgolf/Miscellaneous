4864970a16065b47fee0cf14f505f990cd0c46ea
bravogolfgolf <bggibson@hotmail.com>
2015-02-20 10:42:39 -0500

    Refactor...got Enum working

--- Kata-ArabicToRomanNumbers/src/convert/Arabic.java
+++ Kata-ArabicToRomanNumbers/src/convert/Arabic.java
@@ -4,19 +4,19 @@ public class Arabic {
 
 	public static String convert(int input) {
 		String roman = "";
-		if (input == 5){
-			roman += "V";
-			input-=5;			
+		while (input >= Symbol.V.getValue()){
+			roman += Symbol.V;
+			input-=Symbol.V.getValue();			
 		}
 
-		if (input == 4){
-			roman += "IV";
-			input-=4;			
+		while (input >= Symbol.IV.getValue()){
+			roman += Symbol.IV;
+			input-=Symbol.IV.getValue();			
 		}
 
-		while (input >= 1) {
-			roman += "I";
-			input--;			
+		while (input >= Symbol.I.getValue()) {
+			roman += Symbol.I;
+			input-=Symbol.I.getValue();			
 		}
 		return roman;
 	}
