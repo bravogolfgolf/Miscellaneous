069f1e07fd1b34ab329d04b5607b45c42b730a39
bravogolfgolf <bggibson@hotmail.com>
2015-02-19 20:20:36 -0500

    All tests pass...Complete.

--- Kata-BowlingGame/src/bowlingGame/BowlingGame.java
+++ Kata-BowlingGame/src/bowlingGame/BowlingGame.java
@@ -1,19 +1,46 @@
 package bowlingGame;
 
-import java.util.*;
-
 public class BowlingGame {
 
-	private List<Integer> rolls = new ArrayList<Integer>();
-	private int score = 0;
+	private int rolls[] = new int[21];
+	int currentRoll = 0;
 
 	public void roll(int i) {
-		rolls.add(i);	
+		rolls[currentRoll++] = i;
 	}
 
 	public int score() {
-		for(Integer i : rolls)
-			score+=rolls.get(i);
+		int score = 0;
+		int roll = 0;
+		for(int frame = 1; frame <= 10; frame++)
+			if(isSpare(roll)){
+				score += 10 + spareBonus(roll);
+				roll+=2;
+			}
+			else if(isStrike(roll)){
+				score += 10 + strikeBonus(roll);
+				roll+=1;
+			}
+			else{
+				score += rolls[roll] + rolls[roll+1];
+				roll+=2;}
+		
 		return score;
+	}
+
+	private boolean isStrike(int roll) {
+		return rolls[roll] == 10;
+	}
+
+	private int strikeBonus(int roll) {
+		return rolls[roll+1] + rolls[roll+2];
+	}
+
+	private boolean isSpare(int roll) {
+		return rolls[roll] + rolls[roll+1] == 10;
+	}
+	
+	private int spareBonus(int roll) {
+		return rolls[roll+2];
 	}
 }
