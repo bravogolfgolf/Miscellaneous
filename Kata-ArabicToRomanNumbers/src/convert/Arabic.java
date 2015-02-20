a58ec50e50556ac4a02d3cf0c995bc058e5a8444
bravogolfgolf <bggibson@hotmail.com>
2015-02-19 20:36:44 -0500

    1toI passes

--- Kata-ArabicToRomanNumbers/src/convert/Arabic.java
+++ Kata-ArabicToRomanNumbers/src/convert/Arabic.java
@@ -1,86 +1,8 @@
 package convert;
 
 public class Arabic {
-	int thousandsPlaceHolder = 0;
-	int hundredsPlaceHolder = 0;
-	int tensPlaceHolder = 0;
-	int onesPlaceHolder = 0;
 
-	StringBuffer numbersRepresentingSymbols = new StringBuffer();
-	StringBuffer romanNumber = new StringBuffer();
-
-	public String toRoman(int arabicNumber) {
-		determineVauleOfEachPlaceHolders(arabicNumber);
-		appendProperSymbolsForThousands(thousandsPlaceHolder, 1000);
-		appendProperSymbols(hundredsPlaceHolder, 100);
-		appendProperSymbols(tensPlaceHolder, 10);
-		appendProperSymbols(onesPlaceHolder, 1);
-		createRomanNumber();
-		return romanNumber.toString();
-	}
-
-	private void determineVauleOfEachPlaceHolders(int arabicNumber) {
-		thousandsPlaceHolder = Math.round((arabicNumber / 1000) - 0.5f);
-		hundredsPlaceHolder = Math.round(((arabicNumber - (thousandsPlaceHolder * 1000)) / 100) - 0.5f);
-		tensPlaceHolder = Math.round(((arabicNumber - (thousandsPlaceHolder * 1000) - (hundredsPlaceHolder * 100)) / 10) - 0.5f);
-		onesPlaceHolder = arabicNumber - (thousandsPlaceHolder * 1000) - (hundredsPlaceHolder * 100) - (tensPlaceHolder * 10);
-	}
-
-	private void appendProperSymbolsForThousands(int number, int multiplier) {
-		appendOne(number,multiplier);
-	}
-
-	private void appendProperSymbols(int number, int multiplier) {
-		if (number == 9) {
-			appendOne(1,multiplier);
-			appendTen(multiplier);
-		}
-		else if (number > 5) {
-			int numberOfOnesToAppend = number % 5;
-			appendFive(multiplier);
-			appendOne(numberOfOnesToAppend,multiplier);
-		}
-		else if (number == 5) {
-			appendFive(multiplier);
-		}
-		else if (number == 4) {
-			appendOne(1,multiplier);
-			appendFive(multiplier);
-		}
-		else { appendOne(number,multiplier);
-		}
-	}
-
-	private void appendOne(int numberOfOnesToAppend, int multiplier) {
-		for(int i = 1; i <= numberOfOnesToAppend; i++){
-			Integer numbersRepresentingSymbol = 1 * multiplier;
-			numbersRepresentingSymbols.append(numbersRepresentingSymbol.toString() + ",");		
-		}
-	}
-
-	private void appendFive(int multiplier) {
-		Integer numbersRepresentingSymbol = 5 * multiplier;
-		numbersRepresentingSymbols.append(numbersRepresentingSymbol.toString() + ",");
-	}
-
-	private void appendTen(int multiplier) {
-		Integer numbersRepresentingSymbol = 10 * multiplier;
-		numbersRepresentingSymbols.append(numbersRepresentingSymbol.toString() + ",");
-	}
-
-	private void createRomanNumber() {
-		String commaDelimited = new String(numbersRepresentingSymbols.toString());
-		String[] parts = commaDelimited.split(",");
-		for(int n = 0; n < parts.length; n++) {
-			switch (Integer.parseInt(parts[n])) {
-			case 1: romanNumber.append("I"); break;
-			case 5: romanNumber.append("V"); break;
-			case 10: romanNumber.append("X"); break;
-			case 50: romanNumber.append("L"); break;
-			case 100: romanNumber.append("C"); break;
-			case 500: romanNumber.append("D"); break;
-			case 1000: romanNumber.append("M"); break;
-			}	   
-		}
+	public static String convert(int i) {
+		return "I";
 	}
 }
--- Kata-ArabicToRomanNumbers/tst/convert/ConvertArabicToRomanTest.java
+++ Kata-ArabicToRomanNumbers/tst/convert/ConvertArabicToRomanTest.java
@@ -1,126 +1,13 @@
 package convert;
 
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 
-import convert.Arabic;
-import junit.framework.TestCase;
-
-public class ConvertArabicToRomanTest extends TestCase {
-	Arabic arabic = new Arabic();
-	
-	
-	@Test
-	public void test1toI() throws Exception {
-		assertEquals("I", arabic.toRoman(1));
-	}
+public class ConvertArabicToRomanTest {
 
 	@Test
-	public void test2ToII() throws Exception {
-		assertEquals("II", arabic.toRoman(2));
+	public void test1toI() {
+		assertEquals("I", Arabic.convert(1));
 	}
-
-	@Test
-	public void test3ToIII() throws Exception {
-		assertEquals("III", arabic.toRoman(3));
-	}
-
-	@Test
-	public void test4ToIV() throws Exception {
-		assertEquals("IV", arabic.toRoman(4));
-	}
-
-	@Test
-	public void test5ToV() throws Exception {
-		assertEquals("V", arabic.toRoman(5));
-	}
-
-	@Test
-	public void test6ToVI() throws Exception {
-		assertEquals("VI", arabic.toRoman(6));
-	}
-	@Test
-	public void test7ToVII() throws Exception {
-		assertEquals("VII", arabic.toRoman(7));
-	}
-
-	@Test
-	public void test8ToVIII() throws Exception {
-		assertEquals("VIII", arabic.toRoman(8));
-	}
-	
-	@Test
-	public void test9ToIX() throws Exception {
-		assertEquals("IX", arabic.toRoman(9));
-	}
-
-	@Test
-	public void test10ToX() throws Exception {
-		assertEquals("X", arabic.toRoman(10));
-	}
-	
-	@Test
-	public void test11ToXI() throws Exception {
-		assertEquals("XI", arabic.toRoman(11));
-	}
-	
-	@Test
-	public void test14ToXIV() throws Exception {
-		assertEquals("XIV", arabic.toRoman(14));
-	}
-
-	@Test
-	public void test15ToXV() throws Exception {
-		assertEquals("XV", arabic.toRoman(15));
-	}
-
-	@Test
-	public void test18ToXVIII() throws Exception {
-		assertEquals("XVIII", arabic.toRoman(18));
-	}
-
-	@Test
-	public void test19ToXIX() throws Exception {
-		assertEquals("XIX", arabic.toRoman(19));
-	}
-
-	@Test
-	public void test20ToXX() throws Exception {
-		assertEquals("XX", arabic.toRoman(20));
-	}
-
-	@Test
-	public void test40ToXL() throws Exception {
-		assertEquals("XL", arabic.toRoman(40));
-	}
-
-	@Test
-	public void test90ToXC() throws Exception {
-		assertEquals("XC", arabic.toRoman(90));
-	}
-
-	@Test
-	public void test199ToCXCIX() throws Exception {
-		assertEquals("CXCIX", arabic.toRoman(199));
-	}
-
-	@Test
-	public void test499ToCDCXCIX() throws Exception {
-		assertEquals("CDXCIX", arabic.toRoman(499));
-	}
-	
-	@Test
-	public void test999ToCMXCIX() throws Exception {
-		assertEquals("CMXCIX", arabic.toRoman(999));
-	}
-
-	@Test
-	public void test1000ToM() throws Exception {
-		assertEquals("M", arabic.toRoman(1000));
-	}
-
-	@Test
-	public void test4000ToM() throws Exception {
-		assertEquals("MMMM", arabic.toRoman(4000));
-	}
-
 }
