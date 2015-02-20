440d52760725171be85446f70e4de5a981a05f2a
bravogolfgolf <bggibson@hotmail.com>
2015-02-20 11:32:51 -0500

    All tests pass...Complete.

--- Kata-ArabicToRomanNumbers/tst/convert/ConvertArabicToRomanTest.java
+++ Kata-ArabicToRomanNumbers/tst/convert/ConvertArabicToRomanTest.java
@@ -17,7 +17,7 @@ public class ConvertArabicToRomanTest {
 		checkRomanNumerals("D",500);
 		checkRomanNumerals("M",1000);
 	}
-	
+
 	@Test
 	public void testAddition() {
 		checkRomanNumerals("II",2);
@@ -26,7 +26,7 @@ public class ConvertArabicToRomanTest {
 		checkRomanNumerals("VII",7);
 		checkRomanNumerals("VIII",8);
 	}
-	
+
 	@Test
 	public void testSubtraction() {
 		checkRomanNumerals("IV",4);
@@ -34,10 +34,20 @@ public class ConvertArabicToRomanTest {
 		checkRomanNumerals("XL",40);
 		checkRomanNumerals("XC",90);
 		checkRomanNumerals("CD",400);
-		checkRomanNumerals("CM",900);
-		
+		checkRomanNumerals("CM",900);		
 	}
-	
+
+	@Test
+	public void testAdditionalCases() {
+		checkRomanNumerals("CMXCIX",999);
+		checkRomanNumerals("MCDXLIV",1444);
+		checkRomanNumerals("MCMXLIV",1944);
+		checkRomanNumerals("MCMXLIX",1949);
+		checkRomanNumerals("MCMXCIV",1994);
+		checkRomanNumerals("MCMXCIX",1999);
+		checkRomanNumerals("MM",2000);
+	}
+
 	private void checkRomanNumerals(String roman, int arabic) {
 		assertEquals(roman, convert(arabic));
 	}
