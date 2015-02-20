3648c4ba08a1290cecd58841d23a762b02c111f2
bravogolfgolf <bggibson@hotmail.com>
2015-02-20 11:03:33 -0500

    Refactor out duplication

--- Kata-ArabicToRomanNumbers/src/convert/Arabic.java
+++ Kata-ArabicToRomanNumbers/src/convert/Arabic.java
@@ -4,20 +4,11 @@ public class Arabic {
 
 	public static String convert(int input) {
 		String roman = "";
-		while (input >= Symbol.V.getValue()){
-			roman += Symbol.V;
-			input-=Symbol.V.getValue();			
-		}
-
-		while (input >= Symbol.IV.getValue()){
-			roman += Symbol.IV;
-			input-=Symbol.IV.getValue();			
-		}
-
-		while (input >= Symbol.I.getValue()) {
-			roman += Symbol.I;
-			input-=Symbol.I.getValue();			
-		}
+		for (Symbol symbol : Symbol.values()) {
+			for(;input >= symbol.getValue(); input-=symbol.getValue()){
+				roman += symbol;
+			}
+		}	 
 		return roman;
 	}
 }
