95efcab49587bdade15e115b6b9675e261bb8250
bravogolfgolf <bggibson@hotmail.com>
2015-02-20 20:53:09 -0500

    Refactor

--- Kata-StringCalculator/src/stringCalculator/StringCalculator.java
+++ Kata-StringCalculator/src/stringCalculator/StringCalculator.java
@@ -2,29 +2,38 @@ package stringCalculator;
 
 public class StringCalculator {
 
-	public static int add(String delimited) {
-		int sum = 0;
-		if(isNotEmpty(delimited)){
+	private static String delimited[];
+	private static String delimiterRegularExpression = "";
 
-			String[] defaultDelimiter = delimited.split("[\n]",2);
-			char[] characters= defaultDelimiter[0].toCharArray();
-			String delimiterRegularExpression = "," + "\n";
-			
-			if(characters.length > 2)
-				if( characters[0] == '/' || characters[1] == '/') {
-					delimiterRegularExpression = delimiterRegularExpression + characters[2];
-					System.out.println(delimiterRegularExpression);
-					delimited = defaultDelimiter[1];
-				}
-			System.out.println(delimiterRegularExpression);
-			String[] numbers = delimited.split("[" + delimiterRegularExpression + "]");
-			for( String item : numbers)
-				sum = sum + Integer.parseInt(item.toString());
+	public static int add(String input) {
+		int sum = 0;
+		if(isNotEmpty(input)){		
+			setDelimiterRegularExpression(input);
+			delimited = delimitInput(input);
+			for( String item : delimited)
+				sum += Integer.parseInt(item.toString());
 		}
 		return sum;
 	}
 
 	private static boolean isNotEmpty(String input) {
 		return !input.isEmpty();
+	}
+
+	private static void setDelimiterRegularExpression(String input) {
+		delimiterRegularExpression = "," + "\n";
+		if (input.length() > 2)
+			if (input.substring(0,2).equals("//"))
+				delimiterRegularExpression += input.substring(2,3);
+	}
+	
+	private static String[] delimitInput(String input) {
+		String delimited = input;
+		if (input.length() > 2)
+			if (input.substring(0,2).equals("//")){
+				String[] splitInput = input.split("[\n]",2);
+				delimited = splitInput[1];
+			}
+		return delimited.split("[" + delimiterRegularExpression + "]");
 	}
 }
