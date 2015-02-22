36c385e850526c1802a21f93abf310f622ef37ed
bravogolfgolf <bggibson@hotmail.com>
2015-02-22 11:17:13 -0500

    Refactor after testPrimeFactors8

--- Kata-PrimeFactors/src/primeFactors/PrimeFactors.java
+++ Kata-PrimeFactors/src/primeFactors/PrimeFactors.java
@@ -6,12 +6,12 @@ public class PrimeFactors {
 
 	public static List<Integer> find(int i) {
 		ArrayList<Integer> primeFactors = new ArrayList<Integer>();
-		if( i > 1)
+		if(i > 1)
 			while (i%2 == 0){
 				primeFactors.add(2);
 				i/=2;
 			}
-		if( i > 1)
+		if(i > 1)
 			primeFactors.add(i);
 		return primeFactors;
 	}
