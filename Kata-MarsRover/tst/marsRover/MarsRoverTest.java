7045aa639fd60868d341192f25207fda98ba6a35
bravogolfgolf <bggibson@hotmail.com>
2015-02-23 11:53:58 -0500

    New Project

---
+++ Kata-MarsRover/tst/marsRover/MarsRoverTest.java
@@ -0,0 +1,14 @@
+package marsRover;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MarsRoverTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
