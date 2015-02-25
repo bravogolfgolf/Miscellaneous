ea9e7ebc62113345368e2abdf223bb4c508d1914
bravogolfgolf <bggibson@hotmail.com>
2015-02-24 22:47:27 -0500

    Initial Test of Rover Class

---
+++ Kata-MarsRover/tst/marsRover/RoverTest.java
@@ -0,0 +1,16 @@
+package marsRover;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RoverTest {
+
+	@Test
+	public void testInitialPostion() {
+		Point postion = new Point(21,20);
+		Rover rover = new Rover(postion);
+		assertEquals(postion.getX(),rover.getPostion().getX());
+		assertEquals(postion.getY(),rover.getPostion().getY());
+	}
+}
