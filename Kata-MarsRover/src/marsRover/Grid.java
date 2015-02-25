cbb4f334a4cd7d70c136653080ee2bb4d69fd21e
bravogolfgolf <bggibson@hotmail.com>
2015-02-24 22:08:47 -0500

    Refactored Grid Class

--- Kata-MarsRover/src/marsRover/Grid.java
+++ Kata-MarsRover/src/marsRover/Grid.java
@@ -3,11 +3,9 @@ package marsRover;
 public class Grid {
 
 	public Grid(int i, int j) {
-		// TODO Auto-generated constructor stub
 	}
 
 	public Object getHeight() {
-		// TODO Auto-generated method stub
 		return 10;
 	}
 
