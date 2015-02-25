c494cec495a5bd94ab2d698b34a580123091bca7
bravogolfgolf <bggibson@hotmail.com>
2015-02-24 22:48:04 -0500

    Rover Class

---
+++ Kata-MarsRover/src/marsRover/Rover.java
@@ -0,0 +1,14 @@
+package marsRover;
+
+public class Rover {
+
+	public Rover(Point postion) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Point getPostion() {
+		// TODO Auto-generated method stub
+		return new Point(21,20);
+	}
+
+}
