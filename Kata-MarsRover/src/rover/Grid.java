0882bd8fdc40051cc772755a9d9ef55cd40fe2fc
bravogolfgolf <bggibson@hotmail.com>
2015-02-26 15:24:12 -0500

    Refactored Grid and Grid Test

--- Kata-MarsRover/src/rover/Grid.java
+++ Kata-MarsRover/src/rover/Grid.java
@@ -19,20 +19,31 @@ public class Grid {
 	public int getWidth() {
 		return width;
 	}
+	
+	public void addObstacleAt(int x, int y) {
+		Point obstacle = new Point(x, y);
+		obstacles.add(obstacle);
+	}
 
 	public boolean hasObstacleAt(int x, int y) {
-		if (!obstacles.isEmpty()) {
+		Point input = new Point(x, y);
+		Point currentObstacle;
+		if (isNotEmpty(obstacles)) {
 			for (int i = 0; i < obstacles.size(); i++) {
-				if (obstacles.get(i).getX() == x && obstacles.get(i).getY() == y) {
+				 currentObstacle = obstacles.get(i);
+				if (theTwoAreEqual(input, currentObstacle)) {
 					return true;
 				}
 			}
 		}
 		return false;
 	}
+	
+	private boolean isNotEmpty(List<Point> obstacles) {
+		return !obstacles.isEmpty();
+	}
 
-	public void addObstacleAt(int x, int y) {
-		Point obstacle = new Point(x, y);
-		obstacles.add(obstacle);
+	private boolean theTwoAreEqual(Point input, Point obstacle) {
+		return obstacle.getX() == input.getX() && obstacle.getY() == input.getY();
 	}
 }
--- Kata-MarsRover/tst/rover/GridTest.java
+++ Kata-MarsRover/tst/rover/GridTest.java
@@ -1,7 +1,6 @@
 package rover;
 
 import junit.framework.TestCase;
-
 import org.junit.Test;
 
 public class GridTest extends TestCase{
@@ -9,13 +8,16 @@ public class GridTest extends TestCase{
 	private int height = 10;
 	int width = 11;
 
+	protected void setUp(){
+		createGrid(height, width);
+	}
+
 	private void createGrid(int height, int width) {
 		grid = new Grid(height,width);
 	}
 
 	@Test
 	public void testGridHeightAndWidth() {
-		createGrid(height, width);	
 		assertEquals(10,grid.getHeight());
 		assertEquals(11,grid.getWidth());
 	}
@@ -31,20 +33,17 @@ public class GridTest extends TestCase{
 
 	@Test
 	public void testHasObstacleAt() {
-		createGrid(height, width);
 		assertEquals(false, grid.hasObstacleAt(1, 1));
 	}
-	
+
 	@Test
 	public void testAddObstacleAt() {
-		createGrid(height, width);
 		grid.addObstacleAt(1, 1);
 		assertEquals(true, grid.hasObstacleAt(1, 1));
 	}
-	
+
 	@Test
 	public void testAddMultiObstacleAt() {
-		createGrid(height, width);
 		grid.addObstacleAt(1, 1);
 		grid.addObstacleAt(2, 3);
 		assertEquals(true, grid.hasObstacleAt(1, 1));
