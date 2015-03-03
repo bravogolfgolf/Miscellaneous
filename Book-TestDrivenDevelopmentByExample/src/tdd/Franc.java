60c6fdacfe31ebb70200d7adf719deafd37e8c98
bravogolfgolf <bggibson@hotmail.com>
2015-03-02 22:26:46 -0500

    Chapter 5

---
+++ Book-TestDrivenDevelopmentByExample/src/tdd/Franc.java
@@ -0,0 +1,19 @@
+package tdd;
+
+public class Franc {
+
+	private int amount;
+
+	public Franc(int amount) {
+		this.amount = amount;
+	}
+
+	public Franc times(int multiplier) {
+		return new Franc(amount * multiplier);
+	}
+
+	public boolean equals(Object object) {
+		Franc dollar = (Franc) object;
+		return amount == dollar.amount;
+	}
+}
--- Book-TestDrivenDevelopmentByExample/tst/tdd/DollarTest.java
+++ Book-TestDrivenDevelopmentByExample/tst/tdd/DollarTest.java
@@ -1,15 +1,23 @@
 package tdd;
 
 import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 public class DollarTest {
 
 	@Test
-	public void testMultiplication() {
+	public void testDollarMultiplication() {
 		Dollar five = new Dollar(5);
 		assertEquals(new Dollar(10),five.times(2));
 		assertEquals(new Dollar(15),five.times(3));
+	}
+	
+	@Test
+	public void testFrancMultiplication() {
+		Franc five = new Franc(5);
+		assertEquals(new Franc(10),five.times(2));
+		assertEquals(new Franc(15),five.times(3));
 	}
 	
 	@Test
