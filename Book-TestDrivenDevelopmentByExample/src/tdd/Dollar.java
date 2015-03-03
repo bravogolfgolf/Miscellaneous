3bbc0fa78af2ae8e7d663402e6c9e1d033a4a9c1
bravogolfgolf <bggibson@hotmail.com>
2015-03-02 21:54:57 -0500

    Chapter 1

---
+++ Book-TestDrivenDevelopmentByExample/src/tdd/Dollar.java
@@ -0,0 +1,14 @@
+package tdd;
+
+public class Dollar {
+
+	public int amount;
+
+	public Dollar(int amount) {
+		this.amount = amount;	}
+
+	public void times(int mulyiplier) {
+		amount *= mulyiplier;
+	}
+
+}
