b43db02f4cad16086de49ff5f04d1614ea1c680a
bravogolfgolf <bggibson@hotmail.com>
2015-03-02 22:20:18 -0500

    Chapter 4

--- Book-TestDrivenDevelopmentByExample/src/tdd/Dollar.java
+++ Book-TestDrivenDevelopmentByExample/src/tdd/Dollar.java
@@ -2,15 +2,16 @@ package tdd;
 
 public class Dollar {
 
-	public int amount;
+	private int amount;
 
 	public Dollar(int amount) {
-		this.amount = amount;	}
+		this.amount = amount;
+	}
 
 	public Dollar times(int multiplier) {
 		return new Dollar(amount * multiplier);
 	}
-	
+
 	public boolean equals(Object object) {
 		Dollar dollar = (Dollar) object;
 		return amount == dollar.amount;
