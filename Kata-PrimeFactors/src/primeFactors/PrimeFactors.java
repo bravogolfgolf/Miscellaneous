c84735c338522ebcd8b518a49836731a716c61cc
bravogolfgolf <bggibson@hotmail.com>
2015-03-04 15:46:04 -0500

    Fresh Start

--- Kata-PrimeFactors/src/primeFactors/PrimeFactors.java
+++ Kata-PrimeFactors/src/primeFactors/PrimeFactors.java
@@ -1,14 +1,5 @@
 package primeFactors;
 
-import java.util.*;
-
 public class PrimeFactors {
 
-	public static List<Integer> find(int i) {
-		ArrayList<Integer> primeFactors = new ArrayList<Integer>();
-		for(int divisor = 2; i > 1; divisor++)
-			for (; i % divisor == 0; i /= divisor)
-				primeFactors.add(divisor);
-		return primeFactors;
-	}
 }
