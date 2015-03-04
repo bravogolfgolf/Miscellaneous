f14498a4ca6b1fb6b78d8d0abbc4cb028ed51fb5
bravogolfgolf <bggibson@hotmail.com>
2015-03-04 16:59:08 -0500

    testPrimeFactorsOf9 passes

--- Kata-PrimeFactors/src/primeFactors/PrimeFactors.java
+++ Kata-PrimeFactors/src/primeFactors/PrimeFactors.java
@@ -8,16 +8,12 @@ public class PrimeFactors {
 		ArrayList<Integer> arrayList = new ArrayList<Integer>();
 
 		if(input > 1){
-			while(input % 2 == 0){
-				arrayList.add(2);
-				input /= 2;
+			for(int divisor = 2; input > 1; divisor++){
+				while(input % divisor == 0){
+					arrayList.add(divisor);
+					input /= divisor;
+				}
 			}
-			while(input % 3 == 0){
-				arrayList.add(3);
-				input /= 3;
-			}
-			if(input > 1)
-				arrayList.add(input);						
 		}
 		return arrayList;
 	}
