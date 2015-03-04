28f717172d7f7c4e6af07ea50463914f588681a5
bravogolfgolf <bggibson@hotmail.com>
2015-03-04 15:53:46 -0500

    testPrimeFactorsOf1 passes

--- Kata-PrimeFactors/src/primeFactors/PrimeFactors.java
+++ Kata-PrimeFactors/src/primeFactors/PrimeFactors.java
@@ -2,4 +2,8 @@ package primeFactors;
 
 public class PrimeFactors {
 
+	public static Object calculate(int i) {
+		// TODO Auto-generated method stub
+		return "";
+	}
 }
--- Kata-PrimeFactors/tst/primeFactors/PrimeFactorsTest.java
+++ Kata-PrimeFactors/tst/primeFactors/PrimeFactorsTest.java
@@ -1,5 +1,12 @@
 package primeFactors;
 
+import static org.junit.Assert.*;
+import org.junit.Test;
+
 public class PrimeFactorsTest {
 
+	@Test
+	public void testPrimeFactorsOf1() throws Exception {
+		assertEquals("", PrimeFactors.calculate(1));
+	}
 }
