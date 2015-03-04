1e07ca3d63bb47588edeaa491995213f1b3039ed
bravogolfgolf <bggibson@hotmail.com>
2015-03-04 17:01:40 -0500

    Refactor after testPrimeFactorsOf9

--- Kata-PrimeFactors/src/primeFactors/PrimeFactors.java
+++ Kata-PrimeFactors/src/primeFactors/PrimeFactors.java
@@ -6,15 +6,9 @@ public class PrimeFactors {
 
 	public static List<Integer> calculate(int input) {
 		ArrayList<Integer> arrayList = new ArrayList<Integer>();
-
-		if(input > 1){
-			for(int divisor = 2; input > 1; divisor++){
-				while(input % divisor == 0){
+			for(int divisor = 2; input > 1; divisor++)
+				for(;input % divisor == 0; input /= divisor)
 					arrayList.add(divisor);
-					input /= divisor;
-				}
-			}
-		}
 		return arrayList;
 	}
 }
