9b411959e8addf2a661df27aba9d6d2e7bd92d9b
bravogolfgolf <bggibson@hotmail.com>
2015-03-09 13:01:44 -0400

    Made String getCompassSting() in Direction abstract

--- Kata-MarsRover/src/rover/Direction.java
+++ Kata-MarsRover/src/rover/Direction.java
@@ -1,8 +1,7 @@
 package rover;
 
 abstract class Direction {
-	String getCompassSting(){
-		return null;}
+	abstract String getCompassSting();
 	abstract Direction turnRight();
 	abstract Direction turnLeft();
 	abstract Point goForward(Point position, Grid planet);
