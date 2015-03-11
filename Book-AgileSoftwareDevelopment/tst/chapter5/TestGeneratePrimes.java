ecc1d922c14a2d305d9f7525886cd287f71955ee
bravogolfgolf <bggibson@hotmail.com>
2015-03-11 11:30:14 -0400

    Project setup

---
+++ Book-AgileSoftwareDevelopment/tst/chapter5/TestGeneratePrimes.java
@@ -0,0 +1,14 @@
+package chapter5;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestGeneratePrimes {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
