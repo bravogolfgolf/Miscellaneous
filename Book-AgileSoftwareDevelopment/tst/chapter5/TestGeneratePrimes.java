5609168458b7e26f9f23e29b5a8225b02785c80e
bravogolfgolf <bggibson@hotmail.com>
2015-03-11 12:06:38 -0400

    Typed in Class and Test Class...all tests pass

--- Book-AgileSoftwareDevelopment/tst/chapter5/TestGeneratePrimes.java
+++ Book-AgileSoftwareDevelopment/tst/chapter5/TestGeneratePrimes.java
@@ -7,8 +7,30 @@ import org.junit.Test;
 public class TestGeneratePrimes {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testArrayfor0() {
+		int[] nullArray = GeneratePrimes.generatePrimes(0);
+		assertEquals(nullArray.length, 0);
 	}
 
+	@Test
+	public void testArrayfor2() {
+		int[] minArray = GeneratePrimes.generatePrimes(2);
+		assertEquals(minArray.length, 1);
+		assertEquals(minArray[0], 2);
+	}
+
+	@Test
+	public void testArrayfor3() {
+		int[] threeArray = GeneratePrimes.generatePrimes(3);
+		assertEquals(threeArray.length, 2);
+		assertEquals(threeArray[0], 2);
+		assertEquals(threeArray[1], 3);
+	}
+
+	@Test
+	public void testArrayfor100() {
+		int[] centArray = GeneratePrimes.generatePrimes(100);
+		assertEquals(centArray.length, 25);
+		assertEquals(centArray[24], 97);
+	}
 }
