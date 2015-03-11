90e6e4d44cbeef391215cd731ebf1d279c454240
bravogolfgolf <bggibson@hotmail.com>
2015-03-11 13:11:26 -0400

    Clarified by Renaming variable, Replacing Temp with Query, Inlining Temp

--- Book-AgileSoftwareDevelopment/src/chapter5/PrimesGenerator.java
+++ Book-AgileSoftwareDevelopment/src/chapter5/PrimesGenerator.java
@@ -1,56 +1,53 @@
 package chapter5;
 
 public class PrimesGenerator {
-	static int s;
 	static boolean[] f;
-	static int[] primes;
+	static int[] result;
 
 	public static int[] generatePrimes(int maxValue) {
 
 		if (maxValue < 2)
 			return new int[0];
 		else {
-			initializeSieve(maxValue);
-			sieve();
-			loadPrimes();
-			return primes;
+			initializeArrayOfIntegers(maxValue);
+			crossOutMultiples();
+			putUncrossedIntegersIntoResult();
+			return result;
 		}
 	}
 
-	private static void initializeSieve(int maxValue) {
-		s = maxValue + 1;
-		f = new boolean[s];
-		int i;
-		for(i = 0; i < s; i++)
-			f[i] = true;
+	private static void initializeArrayOfIntegers(int maxValue) {
+		f = new boolean[maxValue + 1];
 		f[0] = f[1] = false;
+		for(int i = 2; i < f.length; i++)
+			f[i] = true;
 	}
 
-	private static void sieve() {
+	private static void crossOutMultiples() {
 		int i;
 		int j;
-		for(i = 2; i < Math.sqrt(s) + 1; i++) {
+		for(i = 2; i < Math.sqrt(f.length) + 1; i++) {
 			if (f[i]) {
-				for (j = 2 * i; j < s; j += i)
+				for (j = 2 * i; j < f.length; j += i)
 					f[j] = false;
 			}
 		}
 	}
 	
-	private static void loadPrimes() {
+	private static void putUncrossedIntegersIntoResult() {
 		int j;
 		int i;
 		
 		int count = 0;
-		for(i = 0; i < s; i++) {
+		for(i = 0; i < f.length; i++) {
 			if (f[i])
 				count++;
 		}
 		
-		primes = new int[count];
-		for (i=0,j=0;i<s;i++) {
+		result = new int[count];
+		for (i = 0,j = 0;i < f.length; i++) {
 			if (f[i])
-				primes[j++] = i;
+				result[j++] = i;
 		}
 	}
 }
