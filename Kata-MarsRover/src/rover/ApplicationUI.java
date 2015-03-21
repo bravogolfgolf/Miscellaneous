ecc93c9fd916c41f1c8eda28ae4c623d1d39aaa0
bravogolfgolf <bggibson@hotmail.com>
2015-03-21 16:32:24 -0400

    Enabled main() in UI

--- Kata-MarsRover/src/rover/ApplicationUI.java
+++ Kata-MarsRover/src/rover/ApplicationUI.java
@@ -3,8 +3,10 @@ package rover;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 //import java.io.InputStreamReader;
 //import java.io.OutputStreamWriter;
+import java.io.OutputStreamWriter;
 
 
 public class ApplicationUI {
@@ -21,20 +23,20 @@ public class ApplicationUI {
 	private BufferedReader reader;
 	private BufferedWriter writer;	
 
-	public ApplicationUI(BufferedReader reader, BufferedWriter writer) {
-		this.reader = reader;
-		this.writer = writer;
+	public ApplicationUI() {
+//		this.reader = reader;
+//		this.writer = writer;
 
-		//		this.reader = new BufferedReader(new InputStreamReader(System.in));
-		//		this.writer = new BufferedWriter(new OutputStreamWriter(System.out));
+		this.reader = new BufferedReader(new InputStreamReader(System.in));
+		this.writer = new BufferedWriter(new OutputStreamWriter(System.out));
 		rover = new Rover(0, 0, 0, "N");
 		grid = new Grid(9, 9);
 		rover.landOnPlanet(grid);
 	}
 
-	//	public static final void main(String[] args) throws IOException {
-	//		new ApplicationUI().run();
-	//	}	
+	public static final void main(String[] args) throws IOException {
+		new ApplicationUI().run();
+	}	
 
 	public void run() throws IOException {
 		String line = "";
--- Kata-MarsRover/tst/rover/ApplicationUITest.java
+++ Kata-MarsRover/tst/rover/ApplicationUITest.java
@@ -16,29 +16,30 @@ public class ApplicationUITest {
 		byte[] buffer = input.toString().getBytes();
 
 		InputStream inputStream = new ByteArrayInputStream(buffer);
-		InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-		BufferedReader reader = new BufferedReader(inputStreamReader);
+//		InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+//		BufferedReader reader = new BufferedReader(inputStreamReader);
 
 		OutputStream outputStream = new ByteArrayOutputStream();
-		OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputStream);
-		BufferedWriter writer = new BufferedWriter(outputStreamWriter);
+//		OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputStream);
+//		BufferedWriter writer = new BufferedWriter(outputStreamWriter);
 
-//		InputStream consoleIn = System.in;
-//		PrintStream consoleOut = System.out;
+		InputStream consoleIn = System.in;
+		PrintStream consoleOut = System.out;
 		
-//		System.setIn(inputStream);
-//		System.setOut(new PrintStream(outputStream));
+		System.setIn(inputStream);
+		System.setOut(new PrintStream(outputStream));
 		
 		try{
 
-			ApplicationUI ui = new ApplicationUI(reader , writer);
+//			ApplicationUI ui = new ApplicationUI(reader , writer);
+			ApplicationUI ui = new ApplicationUI();
 			ui.run();
 
 			assertEquals(expectedOutput.toString(), outputStream.toString());
 		}
 		finally{
-//		System.setIn(consoleIn);
-//		System.setOut(consoleOut);
+		System.setIn(consoleIn);
+		System.setOut(consoleOut);
 		
 		}
 	}
@@ -49,7 +50,6 @@ public class ApplicationUITest {
 		expectedOutput.append(status1(ApplicationUI.INSTRUCTION_PROMPT));
 		input.append(line(ApplicationUI.FORWARD_OPTION));
 		expectedOutput.append(status2(ApplicationUI.STATUS));
-//		expectedOutput.append(status1(ApplicationUI.INSTRUCTION_PROMPT));
 		input.append(line(ApplicationUI.QUIT_OPTION));
 	}
 
