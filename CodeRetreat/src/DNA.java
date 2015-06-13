2fc731ff327a09b93188c9e0b408fb2b4750991e
bravogolfgolf <bggibson@hotmail.com>
2015-06-13 16:40:21 +0200

    GM

--- CodeRetreat/src/DNA.java
+++ CodeRetreat/src/DNA.java
@@ -1,77 +1,32 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-
 
 public class DNA {
 
-	public void process(String inputFilePath, String outputFilePath) throws IOException {
-		String input;
-		String output;
+	private String value;
+	private String characters = "ATGC"; 
 
-		FileInputStream inputFile = new FileInputStream(new File(inputFilePath));
-		FileOutputStream outputFile = new FileOutputStream(new File(outputFilePath));
-
-		BufferedReader reader = new BufferedReader(new InputStreamReader(inputFile));;
-		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outputFile));
-		
-		input = reader.readLine();
-
-		if (input.equals("reverse")){
-			input = reader.readLine();
-			output = removeLetters(input);
-			output = reverseLetters(output);
-			writer.write(output);
-			writer.flush();
-		}
-
-		else {
-			output = removeLetters(input);
-			writer.write(output);
-			writer.flush();
-		}
-		writer.close();
-		reader.close();	
+	public DNA(String value) {
+		this.value = value;
 	}
 
-	private static String removeLetters(String input) {
+	public void clean() {
 		StringBuffer sb = new StringBuffer();
-
-		for (int i = 0; i < input.length(); i++){
-			if(input.charAt(i) == 'A') {
-				sb.append(input.charAt(i));
-			}
-
-			if(input.charAt(i) == 'T') {
-				sb.append(input.charAt(i));
-			}
-
-			if(input.charAt(i) == 'C') {
-				sb.append(input.charAt(i));
-			}
-
-			if(input.charAt(i) == 'G') {
-				sb.append(input.charAt(i));
+		for(int i = 0; i < value.length() - 1; i++) {
+			if(characters.contains(String.valueOf(value.charAt(i)))) {
+				sb.append(value.charAt(i));
 			}
 		}
-
-		return sb.toString();
+		this.value = sb.toString();
 	}
 
-	private static String reverseLetters(String input) {
+	public String getValue() {
+		return this.value;
+	}
 
+	public void reverse() {
 		StringBuffer sb = new StringBuffer();
-
-		for (int i = 0; i < input.length(); i++){
-			sb.append(input.charAt(input.length() - (i+1)));
+		for(int i = value.length() - 1; i >= 0; i--) {
+			sb.append(value.charAt(i));
 		}
-
-		return sb.toString();
+		this.value = sb.toString();
 	}
-	
 }
--- CodeRetreat/tst/TestDNA.java
+++ CodeRetreat/tst/TestDNA.java
@@ -2,46 +2,21 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-
-
-
 public class TestDNA {
 
 	@Test
-	public void testReverse() throws IOException {
-		DNA dna = new DNA();
-		dna.process("inputfile.txt", "outputfile.txt");
-		String outputContent = readFileToString("outputfile.txt");
-		String expectedContent = readFileToString("expectedfile.txt");	
-		assertTrue(outputContent.equals(expectedContent));
+	public void testClean() {
+		DNA dna = new DNA("ATUGACRE");
+		dna.clean();
+		assertEquals("ATGAC", dna.getValue());
 	}
 
-
-	private String readFileToString(String inputFilePath) throws IOException {
-		StringBuilder sb = new StringBuilder();
-		BufferedReader br = new BufferedReader(new FileReader(inputFilePath));
-
-		try {
-
-			String line = br.readLine();
-
-			while (line != null) {
-				sb.append(line);
-				sb.append("\n");
-				line = br.readLine();
-			}
-
-		} catch (Exception e) {
-
-		} finally {
-			br.close();
-		}
-		return sb.toString();
+	@Test
+	public void testReverse() {
+		DNA dna = new DNA("ATUGACRE");
+		dna.reverse();
+		assertEquals("ERCAGUTA", dna.getValue());
 	}
+
 
 }
-
-
