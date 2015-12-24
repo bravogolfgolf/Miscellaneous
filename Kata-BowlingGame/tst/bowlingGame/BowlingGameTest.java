52afbe7514330784db3c0f3ace818dd4eb8d1cd0
bravogolfgolf <bggibson@hotmail.com>
2015-12-24 10:23:36 +0100

    All ones failing test

--- Kata-BowlingGame/tst/bowlingGame/BowlingGameTest.java
+++ Kata-BowlingGame/tst/bowlingGame/BowlingGameTest.java
@@ -11,5 +11,14 @@ public class BowlingGameTest {
 		int score = bg.roll(20,0);
 		assertEquals(0,score);
 	}
+
+	@Test
+	public void rollAllOnes() {
+		BowlingGame bg = new BowlingGame();
+		int score = bg.roll(20,1);
+		assertEquals(20,score);
+	}
+
+	
 	
 }
