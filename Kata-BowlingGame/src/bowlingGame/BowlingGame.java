96b8427a472b5c8d00f6e22ddacddd97a76f9be9
bravogolfgolf <bggibson@hotmail.com>
2015-12-24 10:27:32 +0100

    All ones passes

--- Kata-BowlingGame/src/bowlingGame/BowlingGame.java
+++ Kata-BowlingGame/src/bowlingGame/BowlingGame.java
@@ -3,7 +3,10 @@ package bowlingGame;
 public class BowlingGame {
 
 	public int roll(int roll, int score) {
-		return 0;	
+		int total = 0;
+		for(int i = 0; i < roll; i++)
+			total+=score;
+		return total;	
 	}
 
 }
