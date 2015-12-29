98a3d1f7deb56f68ff3966f30b15538207d3a124
bravogolfgolf <bggibson@hotmail.com>
2015-12-29 10:51:42 +0100

    Start Prime Factors Kata again...

--- Kata-PrimeFactors/src/primeFactors/PrimeFactors.java
+++ Kata-PrimeFactors/src/primeFactors/PrimeFactors.java
@@ -1,14 +1,7 @@
 package primeFactors;
 
-import java.util.*;
 
 public class PrimeFactors {
 
-	public static List<Integer> calculate(int input) {
-		List<Integer> factors = new ArrayList<Integer>();
-		for (int divisor = 2; input > 1; divisor++)
-			for (; input % divisor == 0;input /= divisor) 
-				factors.add(divisor);
-		return factors;
-	}
+	
 }
