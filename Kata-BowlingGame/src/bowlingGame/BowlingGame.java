10f6d9fd3b3c0412fd881d83f0db03a6e69535c1
bravogolfgolf <bggibson@hotmail.com>
2015-12-29 10:16:37 +0100

    refactoring

--- Kata-BowlingGame/src/bowlingGame/BowlingGame.java
+++ Kata-BowlingGame/src/bowlingGame/BowlingGame.java
@@ -12,21 +12,41 @@ public class BowlingGame {
 	public int score(){
 		int total = 0;
 			for(int i = 0, j = 0; j < 10; j++) {
-				if(scoreSheet[i] == 10) {
-					total = total + 10 + scoreSheet[i+1] + scoreSheet[i+2];
+				if(isStrike(i)) {
+					total = total + scoreStrike(i);
 					i = i + 1;
 				}
 
-				else if (scoreSheet[i] + scoreSheet[i+1] == 10) {
-					total = total + 10 + scoreSheet[i+2];
+				else if (isSpare(i)) {
+					total = total + scoreSpare(i);
 					i = i + 2;
 				}
 
 				else {
-					total= total + scoreSheet[i] + scoreSheet[i+1];
+					total= total + scoreFrame(i);
 					i = i + 2;				
 				}
 			}
 		return total;
+	}
+
+	public boolean isStrike(int i) {
+		return scoreSheet[i] == 10;
+	}
+
+	public boolean isSpare(int i) {
+		return scoreSheet[i] + scoreSheet[i+1] == 10;
+	}
+
+	private int scoreStrike(int index) {
+		return 10 + scoreSheet[index+1] + scoreSheet[index+2];		
+	}
+	
+	private int scoreSpare(int index) {
+		return 10 + scoreSheet[index+2];		
+	}
+	
+	private int scoreFrame(int index) {
+		return scoreSheet[index] + scoreSheet[index + 1];
 	}
 }
