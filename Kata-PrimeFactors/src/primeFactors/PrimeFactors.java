6f8440d81169721e012b5d14f94c2428cb47e164
bravogolfgolf <bggibson@hotmail.com>
2015-12-29 13:50:23 +0100

    factor of 4 passes

--- Kata-PrimeFactors/src/primeFactors/PrimeFactors.java
+++ Kata-PrimeFactors/src/primeFactors/PrimeFactors.java
@@ -7,8 +7,14 @@ public class PrimeFactors {
 
 	public static List<Integer> calculate(int number) {
 		List<Integer> list = new ArrayList<Integer>();
-		if(number > 1)
-			list.add(number);
+		if(number > 1) {
+			if(number % 2 == 0) {
+				list.add(2);
+				number = number / 2;				
+			}
+			if(number > 1)
+				list.add(number);
+		}
 		return list;
 	}
 }
