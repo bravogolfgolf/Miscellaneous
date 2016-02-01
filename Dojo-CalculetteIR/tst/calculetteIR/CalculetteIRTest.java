cd7cc037c044433c8369ad876ec4f9299c1a7045
bravogolfgolf <bggibson@hotmail.com>
2016-02-01 13:15:47 +0100

    Test less than 9690 passes

--- Dojo-CalculetteIR/tst/calculetteIR/CalculetteIRTest.java
+++ Dojo-CalculetteIR/tst/calculetteIR/CalculetteIRTest.java
@@ -1,5 +1,18 @@
 package calculetteIR;
 
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+
 public class CalculetteIRTest {
+
+	@Test
+	public void testLevelLessThan9690() {
+		CalculetteIR calculater = new CalculetteIR();
+		int result = calculater.calculate(1000);
+		assertEquals(0, result);
+
+	}
+
 
 }
