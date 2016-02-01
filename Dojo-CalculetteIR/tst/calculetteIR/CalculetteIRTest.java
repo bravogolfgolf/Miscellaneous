e88245ad7562f3e2ef714401336f29b5c2c12d31
bravogolfgolf <bggibson@hotmail.com>
2016-02-01 13:49:45 +0100

    Failing test level 71754

--- Dojo-CalculetteIR/tst/calculetteIR/CalculetteIRTest.java
+++ Dojo-CalculetteIR/tst/calculetteIR/CalculetteIRTest.java
@@ -11,14 +11,19 @@ public class CalculetteIRTest {
 		CalculetteIR calculater = new CalculetteIR();
 		double result = calculater.calculate(1000);
 		assertEquals(0.0, result,0);
-
 	}
+
 	@Test
 	public void testLevelLessThan26764() {
 		CalculetteIR calculater = new CalculetteIR();
 		double result = calculater.calculate(25000);
 		assertEquals(2143.40, result,0);
-
 	}
-
+	
+	@Test
+	public void testLevelLessThan71754() {
+		CalculetteIR calculater = new CalculetteIR();
+		double result = calculater.calculate(50000);
+		assertEquals(9361.16, result,0);
+	}
 }
