21806665e05f9285e61e3dbfe865564aa0115180
bravogolfgolf <bggibson@hotmail.com>
2016-02-03 15:33:43 +0100

    Split one test in two to test values in one and statment format in other; other minor method name changes

--- Kata-VideoStore/tst/videostore/VideoStoreTest.java
+++ Kata-VideoStore/tst/videostore/VideoStoreTest.java
@@ -14,7 +14,7 @@ public class VideoStoreTest {
 	}
 
 	@Test
-	public void testSingleNewReleaseStatement () {
+	public void testSingleNewReleaseStatementTotals() {
 		customer.addRental(new Rental (new Movie ("The Cell", Movie.NEW_RELEASE), 3)); 
 		customer.statement();
 		assertEquals(9.0, customer.getTotal(),.001);
@@ -22,7 +22,7 @@ public class VideoStoreTest {
 	}
 
 	@Test
-	public void testDualNewReleaseStatement () {
+	public void testDualNewReleaseStatementTotals() {
 		customer.addRental (new Rental (new Movie ("The Cell", Movie.NEW_RELEASE), 3));
 		customer.addRental (new Rental (new Movie ("The Tigger Movie", Movie.NEW_RELEASE), 3));		
 		customer.statement();
@@ -31,7 +31,7 @@ public class VideoStoreTest {
 	}
 
 	@Test
-	public void testSingleChildrensStatement () {
+	public void testSingleChildrensStatementTotals() {
 		customer.addRental (new Rental (new Movie ("The Tigger Movie", Movie.CHILDRENS), 3));
 		customer.statement();
 		assertEquals(1.5, customer.getTotal(),.001);
@@ -39,7 +39,17 @@ public class VideoStoreTest {
 	}
 
 	@Test
-	public void testMultipleRegularStatement () {
+	public void testMultipleRegularStatementTotals() {
+		customer.addRental (new Rental (new Movie ("Plan 9 from Outer Space", Movie.REGULAR), 1));
+		customer.addRental (new Rental (new Movie ("8 1/2", Movie.REGULAR), 2));
+		customer.addRental (new Rental (new Movie ("Eraserhead", Movie.REGULAR), 3));		
+		customer.statement();
+		assertEquals(7.5, customer.getTotal(),.001);
+		assertEquals(3, customer.getFrequentRenterPoints());
+	}
+	
+	@Test
+	public void testMultipleRegularStatementOutput() {
 		customer.addRental (new Rental (new Movie ("Plan 9 from Outer Space", Movie.REGULAR), 1));
 		customer.addRental (new Rental (new Movie ("8 1/2", Movie.REGULAR), 2));
 		customer.addRental (new Rental (new Movie ("Eraserhead", Movie.REGULAR), 3));		
@@ -50,8 +60,5 @@ public class VideoStoreTest {
 				+ "You owed 7.5\n"
 				+ "You earned 3 frequent renter points\n", 
 				customer.statement ());
-		customer.statement();
-		assertEquals(7.5, customer.getTotal(),.001);
-		assertEquals(3, customer.getFrequentRenterPoints());
 	}
 }
