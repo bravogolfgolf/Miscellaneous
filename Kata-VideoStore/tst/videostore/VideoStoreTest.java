5f2fa102dbbc8308de45787c15fb01963acc7d71
bravogolfgolf <bggibson@hotmail.com>
2016-02-03 16:09:28 +0100

    Generalized movie types and moved them up to fields in class

--- Kata-VideoStore/tst/videostore/VideoStoreTest.java
+++ Kata-VideoStore/tst/videostore/VideoStoreTest.java
@@ -7,15 +7,27 @@ import static org.junit.Assert.*;
 public class VideoStoreTest {
 
 	private Statement statement;
+	private Movie newRelease1;
+	private Movie newRelease2;
+	private Movie children;
+	private Movie regular1;
+	private Movie regular2;
+	private Movie regular3;
 
 	@Before
 	public void setUp ()  {
-		statement= new Statement ("Fred");
+		statement= new Statement ("Customer");
+		newRelease1 = new Movie ("New Release 1", Movie.NEW_RELEASE);
+		newRelease2 = new Movie ("New Release 2", Movie.NEW_RELEASE);
+		children = new Movie ("Children", Movie.CHILDRENS);
+		regular1 = new Movie ("Regular 1", Movie.REGULAR);
+		regular2 = new Movie ("Regular 2", Movie.REGULAR);
+		regular3 = new Movie ("Regular 3", Movie.REGULAR);
 	}
 
 	@Test
 	public void testSingleNewReleaseStatementTotals() {
-		statement.addRental(new Rental (new Movie ("The Cell", Movie.NEW_RELEASE), 3)); 
+		statement.addRental(new Rental (newRelease1, 3)); 
 		statement.generate();
 		assertEquals(9.0, statement.getTotal(),.001);
 		assertEquals(2, statement.getFrequentRenterPoints());
@@ -23,8 +35,8 @@ public class VideoStoreTest {
 
 	@Test
 	public void testDualNewReleaseStatementTotals() {
-		statement.addRental (new Rental (new Movie ("The Cell", Movie.NEW_RELEASE), 3));
-		statement.addRental (new Rental (new Movie ("The Tigger Movie", Movie.NEW_RELEASE), 3));		
+		statement.addRental (new Rental (newRelease1, 3));
+		statement.addRental (new Rental (newRelease2, 3));		
 		statement.generate();
 		assertEquals(18.0, statement.getTotal(),.001);
 		assertEquals(4, statement.getFrequentRenterPoints());
@@ -32,7 +44,7 @@ public class VideoStoreTest {
 
 	@Test
 	public void testSingleChildrensStatementTotals() {
-		statement.addRental (new Rental (new Movie ("The Tigger Movie", Movie.CHILDRENS), 3));
+		statement.addRental (new Rental (children, 3));
 		statement.generate();
 		assertEquals(1.5, statement.getTotal(),.001);
 		assertEquals(1, statement.getFrequentRenterPoints());
@@ -40,9 +52,9 @@ public class VideoStoreTest {
 
 	@Test
 	public void testMultipleRegularStatementTotals() {
-		statement.addRental (new Rental (new Movie ("Plan 9 from Outer Space", Movie.REGULAR), 1));
-		statement.addRental (new Rental (new Movie ("8 1/2", Movie.REGULAR), 2));
-		statement.addRental (new Rental (new Movie ("Eraserhead", Movie.REGULAR), 3));		
+		statement.addRental (new Rental (regular1, 1));
+		statement.addRental (new Rental (regular2, 2));
+		statement.addRental (new Rental (regular3, 3));		
 		statement.generate();
 		assertEquals(7.5, statement.getTotal(),.001);
 		assertEquals(3, statement.getFrequentRenterPoints());
@@ -50,13 +62,13 @@ public class VideoStoreTest {
 	
 	@Test
 	public void testMultipleRegularStatementOutput() {
-		statement.addRental (new Rental (new Movie ("Plan 9 from Outer Space", Movie.REGULAR), 1));
-		statement.addRental (new Rental (new Movie ("8 1/2", Movie.REGULAR), 2));
-		statement.addRental (new Rental (new Movie ("Eraserhead", Movie.REGULAR), 3));		
-		assertEquals ("Rental Record for Fred\n"
-				+ "\tPlan 9 from Outer Space\t2.0\n"
-				+ "\t8 1/2\t2.0\n"
-				+ "\tEraserhead\t3.5\n"
+		statement.addRental (new Rental (regular1, 1));
+		statement.addRental (new Rental (regular2, 2));
+		statement.addRental (new Rental (regular3, 3));		
+		assertEquals ("Rental Record for Customer\n"
+				+ "\tRegular 1\t2.0\n"
+				+ "\tRegular 2\t2.0\n"
+				+ "\tRegular 3\t3.5\n"
 				+ "You owed 7.5\n"
 				+ "You earned 3 frequent renter points\n", 
 				statement.generate ());
