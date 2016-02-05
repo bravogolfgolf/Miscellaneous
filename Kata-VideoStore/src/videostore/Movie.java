bf56846f181280aed8d924e987b220b06f1962b6
bravogolfgolf <bggibson@hotmail.com>
2016-02-05 12:52:56 +0100

    moved determineAmount to Movie

--- Kata-VideoStore/src/videostore/Movie.java
+++ Kata-VideoStore/src/videostore/Movie.java
@@ -27,5 +27,24 @@ public class Movie
 	public String getTitle () {
 		return title;
 	}
-	
+
+	double determineAmount(int daysRented) {
+        double rentalAmount = 0;
+        switch (getPriceCode()) {
+            case REGULAR:
+                rentalAmount += 2;
+                if (daysRented > 2)
+                    rentalAmount += (daysRented - 2) * 1.5;
+                break;
+            case NEW_RELEASE:
+                rentalAmount += daysRented * 3;
+                break;
+            case CHILDRENS:
+                rentalAmount += 1.5;
+                if (daysRented > 3)
+                    rentalAmount += (daysRented - 3) * 1.5;
+                break;
+        }
+        return rentalAmount;
+    }
 }
--- Kata-VideoStore/src/videostore/Rental.java
+++ Kata-VideoStore/src/videostore/Rental.java
@@ -1,49 +1,28 @@
 package videostore;
 
-public class Rental
-{
-	private Movie movie;
-	private int daysRented;
+public class Rental {
+    private Movie movie;
+    private int daysRented;
 
-	public Rental (Movie movie, int daysRented) {
-		this.movie = movie;
-		this.daysRented = daysRented;
-	}
-
-	public Movie getMovie () {
-		return movie;
-	}
-
-	public String getTitle() {
-		return movie.getTitle();
-	}
-
-	public int getPriceCode() {
-		return movie.getPriceCode();
-	}
-
-	double determineAmount() {
-        double rentalAmount = 0;
-        switch (getPriceCode()) {
-            case Movie.REGULAR:
-                rentalAmount += 2;
-				if (daysRented > 2)
-					rentalAmount += (daysRented - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-				rentalAmount += daysRented * 3;
-                break;
-            case Movie.CHILDRENS:
-                rentalAmount += 1.5;
-				if (daysRented > 3)
-					rentalAmount += (daysRented - 3) * 1.5;
-                break;
-        }
-        return rentalAmount;
+    public Rental(Movie movie, int daysRented) {
+        this.movie = movie;
+        this.daysRented = daysRented;
     }
 
-	int determineFrequentRenterPoints() {
-		boolean bonusIsEarned = getPriceCode() == Movie.NEW_RELEASE && daysRented > 1;
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public String getTitle() {
+        return movie.getTitle();
+    }
+
+    public double determineAmount() {
+        return movie.determineAmount(daysRented);
+    }
+
+    int determineFrequentRenterPoints() {
+        boolean bonusIsEarned = movie.getPriceCode() == Movie.NEW_RELEASE && daysRented > 1;
         if (bonusIsEarned)
             return 2;
         return 1;
