ca2d2840146a8545557a6cb244a472b758311967
bravogolfgolf <bggibson@hotmail.com>
2016-02-05 12:19:56 +0100

    Broke generate method up into many different function for readabilty

--- Kata-VideoStore/src/videostore/Statement.java
+++ Kata-VideoStore/src/videostore/Statement.java
@@ -4,79 +4,92 @@ package videostore;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Statement 
-{
-	private String customerName;
-	private List<Rental> rentals = new ArrayList<Rental>();
-	private double totalAmount;
-	private int frequentRenterPoints;
-	
-	public Statement (String name) {
+public class Statement {
+    private String customerName;
+    private List<Rental> rentals = new ArrayList<Rental>();
+    private double totalAmount;
+    private int frequentRenterPoints;
 
-		this.customerName = name;
-	}
+    public Statement(String name) {
+        this.customerName = name;
+    }
 
-	public void addRental (Rental rental) {
+    public void addRental(Rental rental) {
+        rentals.add(rental);
+    }
 
-		rentals.add (rental);
-	}
+    public String generate() {
+        clearTotals();
+        String statementText = header();
+        statementText += rentalLines();
+        statementText += footer();
+        return statementText;
+    }
 
-	public String getName () {
+    private void clearTotals() {
+        totalAmount = 0;
+        frequentRenterPoints = 0;
+    }
 
-		return customerName;
-	}
+    private String header() {
+        return String.format("Rental Record for %s\n", customerName);
+    }
 
-	public String generate () {
-		totalAmount = 0;
-		frequentRenterPoints = 0;
+    private String rentalLines() {
+        String rentalLines = "";
+        for (Rental rental : rentals)
+            rentalLines += rentalLine(rental);
+        return rentalLines;
+    }
 
-		String result = "Rental Record for " + getName () + "\n";
+    private String rentalLine(Rental rental) {
+        double rentalAmount = determineAmount(rental);
+        frequentRenterPoints += determineFrequentRenterPoints(rental);
+        totalAmount += rentalAmount;
+        return formatRentalLine(rental, rentalAmount);
+    }
 
-		for (Rental each:rentals	) {
-			double thisAmount = 0;
+    private double determineAmount(Rental rental) {
+        double rentalAmount = 0;
+        switch (rental.getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                rentalAmount += 2;
+                if (rental.getDaysRented() > 2)
+                    rentalAmount += (rental.getDaysRented() - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                rentalAmount += rental.getDaysRented() * 3;
+                break;
+            case Movie.CHILDRENS:
+                rentalAmount += 1.5;
+                if (rental.getDaysRented() > 3)
+                    rentalAmount += (rental.getDaysRented() - 3) * 1.5;
+                break;
+        }
+        return rentalAmount;
+    }
 
-			// determines the amount for each line
-			switch (each.getMovie ().getPriceCode ()) {
-			case Movie.REGULAR:
-				thisAmount += 2;
-				if (each.getDaysRented () > 2)
-					thisAmount += (each.getDaysRented () - 2) * 1.5;
-				break;
-			case Movie.NEW_RELEASE:
-				thisAmount += each.getDaysRented () * 3;
-				break;
-			case Movie.CHILDRENS:
-				thisAmount += 1.5;
-				if (each.getDaysRented () > 3)
-					thisAmount += (each.getDaysRented () - 3) * 1.5;
-				break;
-			}
+    private int determineFrequentRenterPoints(Rental rental) {
+        boolean bounsIsEarned = rental.getMovie().getPriceCode() == Movie.NEW_RELEASE && rental.getDaysRented() > 1;
+        if (bounsIsEarned)
+            return 2;
+        return 1;
+    }
 
-			frequentRenterPoints++;
+    private String formatRentalLine(Rental rental, double rentalAmount) {
+        return String.format("\t%s\t%.1f\n", rental.getMovie().getTitle(), rentalAmount);
+    }
 
-			if (each.getMovie ().getPriceCode () == Movie.NEW_RELEASE 
-					&& each.getDaysRented () > 1)
-				frequentRenterPoints++;
+    private String footer() {
+        return String.format(
+                "You owed %.1f\nYou earned %d frequent renter points\n", totalAmount, frequentRenterPoints);
+    }
 
-			result += "\t" + each.getMovie ().getTitle () + "\t"
-					+ String.valueOf (thisAmount) + "\n";
-			totalAmount += thisAmount;
+    public double getTotal() {
+        return totalAmount;
+    }
 
-		}
-
-		result += "You owed " + String.valueOf (totalAmount) + "\n";
-		result += "You earned " + String.valueOf (frequentRenterPoints) + " frequent renter points\n";
-
-
-		return result;
-	}
-
-
-	public double getTotal() {
-		return totalAmount;
-	}
-
-	public int getFrequentRenterPoints() {
-		return frequentRenterPoints;
-	}
+    public int getFrequentRenterPoints() {
+        return frequentRenterPoints;
+    }
 }
