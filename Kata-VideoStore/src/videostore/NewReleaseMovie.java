0be36911f2d921a71920a82f204f3bfde670d2bf
bravogolfgolf <bggibson@hotmail.com>
2016-02-05 13:15:14 +0100

    pushed down determineAmount and determineFrequentRenterPoints; made priceCode package accessable; formated code

---
+++ Kata-VideoStore/src/videostore/NewReleaseMovie.java
@@ -0,0 +1,36 @@
+package videostore;
+
+public class NewReleaseMovie extends Movie {
+    public NewReleaseMovie(String title) {
+        super(title, Movie.NEW_RELEASE);
+    }
+
+    @Override
+    double determineAmount(int daysRented) {
+        double rentalAmount = 0;
+        switch (priceCode) {
+            case REGULAR:
+                rentalAmount += 2;
+                if (daysRented > 2)
+                    rentalAmount += (daysRented - 2) * 1.5;
+                break;
+            case NEW_RELEASE:
+                rentalAmount += daysRented * 3;
+                break;
+            case CHILDRENS:
+                rentalAmount += 1.5;
+                if (daysRented > 3)
+                    rentalAmount += (daysRented - 3) * 1.5;
+                break;
+        }
+        return rentalAmount;
+    }
+
+    @Override
+    int determineFrequentRenterPoints(int daysRented) {
+        boolean bonusIsEarned = (priceCode == NEW_RELEASE && daysRented > 1);
+        if (bonusIsEarned)
+            return 2;
+        return 1;
+    }
+}
--- Kata-VideoStore/tst/videostore/VideoStoreTest.java
+++ Kata-VideoStore/tst/videostore/VideoStoreTest.java
@@ -2,76 +2,77 @@ package videostore;
 
 import org.junit.Before;
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class VideoStoreTest {
 
-	public static final double DELTA = .001;
-	private Statement statement;
-	private Movie newRelease1;
-	private Movie newRelease2;
-	private Movie children;
-	private Movie regular1;
-	private Movie regular2;
-	private Movie regular3;
+    public static final double DELTA = .001;
+    private Statement statement;
+    private Movie newRelease1;
+    private Movie newRelease2;
+    private Movie children;
+    private Movie regular1;
+    private Movie regular2;
+    private Movie regular3;
 
-	@Before
-	public void setUp ()  {
-		statement= new Statement ("Customer");
-		newRelease1 = new Movie ("New Release 1", Movie.NEW_RELEASE);
-		newRelease2 = new Movie ("New Release 2", Movie.NEW_RELEASE);
-		children = new Movie ("Children", Movie.CHILDRENS);
-		regular1 = new Movie ("Regular 1", Movie.REGULAR);
-		regular2 = new Movie ("Regular 2", Movie.REGULAR);
-		regular3 = new Movie ("Regular 3", Movie.REGULAR);
-	}
+    @Before
+    public void setUp() {
+        statement = new Statement("Customer");
+        newRelease1 = new NewReleaseMovie("New Release 1");
+        newRelease2 = new NewReleaseMovie("New Release 2");
+        children = new ChildrensMovie("Children");
+        regular1 = new RegularMovie("Regular 1");
+        regular2 = new RegularMovie("Regular 2");
+        regular3 = new RegularMovie("Regular 3");
+    }
 
-	@Test
-	public void testSingleNewReleaseStatementTotals() {
-		statement.addRental(new Rental (newRelease1, 3)); 
-		statement.generate();
-		assertEquals(9.0, statement.getTotal(), DELTA);
-		assertEquals(2, statement.getFrequentRenterPoints());
-	}
+    @Test
+    public void testSingleNewReleaseStatementTotals() {
+        statement.addRental(new Rental(newRelease1, 3));
+        statement.generate();
+        assertEquals(9.0, statement.getTotal(), DELTA);
+        assertEquals(2, statement.getFrequentRenterPoints());
+    }
 
-	@Test
-	public void testDualNewReleaseStatementTotals() {
-		statement.addRental (new Rental (newRelease1, 3));
-		statement.addRental (new Rental (newRelease2, 3));		
-		statement.generate();
-		assertEquals(18.0, statement.getTotal(), DELTA);
-		assertEquals(4, statement.getFrequentRenterPoints());
-	}
+    @Test
+    public void testDualNewReleaseStatementTotals() {
+        statement.addRental(new Rental(newRelease1, 3));
+        statement.addRental(new Rental(newRelease2, 3));
+        statement.generate();
+        assertEquals(18.0, statement.getTotal(), DELTA);
+        assertEquals(4, statement.getFrequentRenterPoints());
+    }
 
-	@Test
-	public void testSingleChildrensStatementTotals() {
-		statement.addRental (new Rental (children, 3));
-		statement.generate();
-		assertEquals(1.5, statement.getTotal(), DELTA);
-		assertEquals(1, statement.getFrequentRenterPoints());
-	}
+    @Test
+    public void testSingleChildrensStatementTotals() {
+        statement.addRental(new Rental(children, 3));
+        statement.generate();
+        assertEquals(1.5, statement.getTotal(), DELTA);
+        assertEquals(1, statement.getFrequentRenterPoints());
+    }
 
-	@Test
-	public void testMultipleRegularStatementTotals() {
-		statement.addRental (new Rental (regular1, 1));
-		statement.addRental (new Rental (regular2, 2));
-		statement.addRental (new Rental (regular3, 3));		
-		statement.generate();
-		assertEquals(7.5, statement.getTotal(), DELTA);
-		assertEquals(3, statement.getFrequentRenterPoints());
-	}
-	
-	@Test
-	public void testMultipleRegularStatementOutput() {
-		statement.addRental (new Rental (regular1, 1));
-		statement.addRental (new Rental (regular2, 2));
-		statement.addRental (new Rental (regular3, 3));		
-		assertEquals ("Rental Record for Customer\n"
-				+ "\tRegular 1\t2.0\n"
-				+ "\tRegular 2\t2.0\n"
-				+ "\tRegular 3\t3.5\n"
-				+ "You owed 7.5\n"
-				+ "You earned 3 frequent renter points\n", 
-				statement.generate ());
-	}
+    @Test
+    public void testMultipleRegularStatementTotals() {
+        statement.addRental(new Rental(regular1, 1));
+        statement.addRental(new Rental(regular2, 2));
+        statement.addRental(new Rental(regular3, 3));
+        statement.generate();
+        assertEquals(7.5, statement.getTotal(), DELTA);
+        assertEquals(3, statement.getFrequentRenterPoints());
+    }
+
+    @Test
+    public void testMultipleRegularStatementOutput() {
+        statement.addRental(new Rental(regular1, 1));
+        statement.addRental(new Rental(regular2, 2));
+        statement.addRental(new Rental(regular3, 3));
+        assertEquals("Rental Record for Customer\n"
+                        + "\tRegular 1\t2.0\n"
+                        + "\tRegular 2\t2.0\n"
+                        + "\tRegular 3\t3.5\n"
+                        + "You owed 7.5\n"
+                        + "You earned 3 frequent renter points\n",
+                statement.generate());
+    }
 }
