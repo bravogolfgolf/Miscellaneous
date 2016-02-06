b842201dc20d86b85a1b79d900e12aeeb240ef5d
bravogolfgolf <bggibson@hotmail.com>
2016-02-06 02:00:32 +0100

    added Player class

---
+++ Kata-Monopoly/src/Player.java
@@ -0,0 +1,13 @@
+public class Player {
+
+    private String token = "";
+
+
+    public Player(String token) {
+        this.token = token;
+    }
+
+    public String getPlayerName() {
+        return token;
+    }
+}
--- Kata-Monopoly/tst/GameTest.java
+++ Kata-Monopoly/tst/GameTest.java
@@ -1,13 +1,33 @@
+import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
-
 import static org.junit.Assert.*;
 
 
 public class GameTest {
 
-    @Test
-    public void testGame() {
-        Game board = new Game();
-        assertEquals(40, board.getSize());
+    private Game game;
+
+    @Before
+    public void setup(){
+        game = new Game();
     }
+
+    @Test
+    public void testGameBoardSize() {
+        assertEquals(40, game.getBoardSize());
+    }
+
+    @Test
+    public void testCreatePlayer(){
+        Player player = new Player("Cat");
+        assertEquals("Cat", player.getPlayerName());
+    }
+
+//    @Test
+//    public void testMovePlayer(){
+//        game.addPlayer("Cat");
+//        player.roll();
+//        assertEquals(7,player.getCurrentPosition());
+//    }
 }
