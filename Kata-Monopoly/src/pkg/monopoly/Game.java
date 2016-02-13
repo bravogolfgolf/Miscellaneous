eb5e64009b60a2107ef9095b773084c203181024
bravogolfgolf <bggibson@hotmail.com>
2016-02-13 11:04:47 +0100

    First UI test paasses

--- Kata-Monopoly/src/pkg/monopoly/Game.java
+++ Kata-Monopoly/src/pkg/monopoly/Game.java
@@ -1,9 +1,26 @@
 package pkg.monopoly;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+
 public class Game {
 
-    public static void main(String[] args) {
+    private final BufferedReader reader;
+    private final BufferedWriter writer;
 
-        System.out.println("How many player (2-8)?");
+    public Game(BufferedReader reader, BufferedWriter writer) {
+        this.reader = reader;
+        this.writer = writer;
+    }
+
+    public void run() throws IOException {
+        write("How many player (2-8)?");
+
+    }
+
+    private void write(String s) throws IOException {
+        writer.write(s,0,s.length());
+        writer.flush();
     }
 }
--- Kata-Monopoly/tst/pkg/monopoly/GameTest.java
+++ Kata-Monopoly/tst/pkg/monopoly/GameTest.java
@@ -2,13 +2,36 @@ package pkg.monopoly;
 
 import org.junit.Test;
 
+import java.io.*;
+
+import static org.junit.Assert.assertEquals;
+
 public class GameTest {
 
-    private Game game;
-
     @Test
-    public void testAddTokenToGame() {
-        Game.main(null);
+    public void testRequestAProperNumberOfPlayersForGame() throws IOException {
+        StringBuffer expectedOutput = new StringBuffer();
+        StringBuffer input = new StringBuffer();
+        setup(expectedOutput, input);
+
+        byte[] buffer = input.toString().getBytes();
+        InputStream inputStream = new ByteArrayInputStream(buffer);
+        InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+        BufferedReader reader = new BufferedReader(inputStreamReader);
+
+        OutputStream outputStream = new ByteArrayOutputStream();
+        OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputStream);
+        BufferedWriter writer = new BufferedWriter(outputStreamWriter);
+
+        Game game = new Game(reader,writer);
+        game.run();
+
+        assertEquals(expectedOutput.toString(),outputStream.toString());
+
+    }
+
+    private void setup(StringBuffer expectedOutput, StringBuffer input) {
+        expectedOutput.append("How many player (2-8)?");
     }
 
 }
