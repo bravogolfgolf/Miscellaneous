250013ef4f4d00b90ad4f9342c2c50e459b74837
bravogolfgolf <bggibson@hotmail.com>
2016-02-15 16:14:25 +0100

    testGameWithTwoPlayers passes

--- Kata-Monopoly/src/pkg/monopoly/Game.java
+++ Kata-Monopoly/src/pkg/monopoly/Game.java
@@ -1,34 +1,11 @@
 package pkg.monopoly;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
 import java.util.*;
 
 public class Game {
 
     public static final Die die = new Die();
-    public static final String HOW_MANY_PLAYERS = "How many player (2-8)?";
-    public static final String INVALID_NUMBER_OF_PLAYERS = "Please select 2 to 8 players.";
-    public static final String SELECT_TOKEN = "Select token for player %d.";
-    public static final String INVALID_TOKEN_LETTER = "Please select a valid letter corresponding to one of the remaining tokens.";
-    public static final int MINIMUM_NUMBER_OF_PLAYERS = 2;
-    public static final int MAXIMUM_NUMBER_OF_PLAYERS = 8;
-    private final BufferedReader reader;
-    private final BufferedWriter writer;
-    private List<MenuItems> tokenMenuItems = new ArrayList<MenuItems>(
-            Arrays.asList(MenuItems.Boot, MenuItems.Car, MenuItems.Cat, MenuItems.Dog, MenuItems.Hat, MenuItems.Ship, MenuItems.Thimble, MenuItems.Wheelbarrow));
     private List<Player> players = new ArrayList<Player>();
-    private int numberOfRounds = 0;
-
-    public Game(BufferedReader reader, BufferedWriter writer) {
-        this.reader = reader;
-        this.writer = writer;
-    }
-
-    public Player getPlayer(int index) {
-        return players.get(index);
-    }
 
     public void addPlayer(Player player) {
         players.add(player);
@@ -36,114 +13,5 @@ public class Game {
 
     public int getNumberOfPlayers() {
         return players.size();
-    }
-
-    public int getNumberOfRounds() {
-        return numberOfRounds;
-    }
-
-    public void run() throws IOException {
-        gameSetup();
-
-        do {
-            playRound();
-        } while (numberOfRounds < 20);
-    }
-
-    private void write(String s) throws IOException {
-        writer.write(s, 0, s.length());
-        writer.flush();
-    }
-
-    private void gameSetup() throws IOException {
-        int numberOfPlayers = determineNumberOfPlayers();
-        playersChooseTokens(numberOfPlayers);
-        randomizeStartingOrder();
-    }
-
-    private int determineNumberOfPlayers() throws IOException {
-        String line;
-        int numberOfPlayers;
-        boolean unacceptableNumberOfPlayersEntered = true;
-
-        do {
-            write(HOW_MANY_PLAYERS);
-            line = reader.readLine();
-            numberOfPlayers = Integer.parseInt(line);
-            if (numberOfPlayers >= MINIMUM_NUMBER_OF_PLAYERS && numberOfPlayers <= MAXIMUM_NUMBER_OF_PLAYERS) {
-                unacceptableNumberOfPlayersEntered = false;
-            } else {
-                write(INVALID_NUMBER_OF_PLAYERS);
-            }
-        } while (unacceptableNumberOfPlayersEntered);
-        return numberOfPlayers;
-    }
-
-    private void playersChooseTokens(int numberOfPlayers) throws IOException {
-
-        for (int playerNumber = 1; playerNumber <= numberOfPlayers; playerNumber++) {
-            int tokenIndex;
-            tokenIndex = getIndexOfTokenSelectedBy(playerNumber);
-            createPlayerWithToken(tokenIndex);
-            removeTokenSoFollowingPlayerManyNotChooseIt(tokenIndex);
-        }
-    }
-
-    private int getIndexOfTokenSelectedBy(int playerNumber) throws IOException {
-        String line;
-        String tokenLetter;
-        int tokenIndex = -1;
-        boolean unacceptableTokenLetterIsEntered = true;
-
-        do {
-            write(String.format(SELECT_TOKEN, playerNumber));
-            writeRemainingTokens();
-            line = reader.readLine();
-            tokenLetter = line.toUpperCase();
-
-            for (int counter = 0; counter < tokenMenuItems.size(); counter++) {
-                if (tokenMenuItems.get(counter).getTokenLetter().equals(tokenLetter)) {
-                    tokenIndex = counter;
-                    unacceptableTokenLetterIsEntered = false;
-                }
-            }
-
-            if (unacceptableTokenLetterIsEntered)
-                write(INVALID_TOKEN_LETTER);
-
-        } while (unacceptableTokenLetterIsEntered);
-        return tokenIndex;
-    }
-
-    private void writeRemainingTokens() throws IOException {
-        String commaSpace = ", ";
-        String remainingTokens = tokenMenuItems.get(0).getMenuString();
-        for (int index = 1; index < tokenMenuItems.size(); index++) {
-            remainingTokens += commaSpace;
-            remainingTokens += tokenMenuItems.get(index).getMenuString();
-        }
-        write(remainingTokens);
-    }
-
-    private void createPlayerWithToken(int tokenIndex) {
-        String label = tokenMenuItems.get(tokenIndex).getDescription();
-        Token token = new Token(label);
-        Player player = new Player(token);
-        addPlayer(player);
-    }
-
-    private void removeTokenSoFollowingPlayerManyNotChooseIt(int tokenIndex) {
-        tokenMenuItems.remove(tokenIndex);
-    }
-
-    private void randomizeStartingOrder() {
-        Collections.shuffle(players);
-    }
-
-    private void playRound() throws IOException {
-        for (Player player : players) {
-            player.incrementTurn();
-        }
-        numberOfRounds++;
     }
 }
--- Kata-Monopoly/tst/pkg/monopoly/GameTest.java
+++ Kata-Monopoly/tst/pkg/monopoly/GameTest.java
@@ -1,132 +1,49 @@
 package pkg.monopoly;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.*;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 public class GameTest {
 
-    private final int FIRST_PLAYER = 0;
-    private final int SECOND_PLAYER = 1;
-    private final int PLAYER_ONE = 1;
-    private final int PLAYER_TWO = 2;
+    private Game game;
 
-    private final StringBuffer expectedOutput = new StringBuffer();
-    private final StringBuffer input = new StringBuffer();
-    private final OutputStream outputStream = new ByteArrayOutputStream();
+    @Before
+    public void setUp() throws Exception {
+        Token boot = new Token("Boot");
+        Token cat = new Token("Cat");
+        Player player1 = new Player(boot);
+        Player player2 = new Player(cat);
+        game = new Game();
+        game.addPlayer(player1);
+        game.addPlayer(player2);
 
-    private BufferedReader getBufferedReader() {
-        byte[] buffer = input.toString().getBytes();
-        InputStream inputStream = new ByteArrayInputStream(buffer);
-        InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-        return new BufferedReader(inputStreamReader);
-    }
-
-    private BufferedWriter getBufferedWriter() {
-        OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputStream);
-        return new BufferedWriter(outputStreamWriter);
-    }
-
-    private Game createGame() throws IOException {
-        BufferedReader reader = getBufferedReader();
-        BufferedWriter writer = getBufferedWriter();
-        return getGame(reader, writer);
-    }
-
-    private Game getGame(BufferedReader reader, BufferedWriter writer) throws IOException {
-        Game game = new Game(reader, writer);
-        game.run();
-        return game;
-    }
-
-    private String line(String input) {
-        return String.format("%s%n", input);
     }
 
     @Test
-    public void testCreatePlayersUI() throws IOException {
+    public void testGameWithTwoPlayers() throws IOException {
         final int EXPECTED_NUMBER_OF_PLAYERS = 2;
-
-        setupUserTestScript(expectedOutput, input);
-        Game game = createGame();
-        assertEquals(expectedOutput.toString(), outputStream.toString());
         assertEquals(EXPECTED_NUMBER_OF_PLAYERS, game.getNumberOfPlayers());
     }
 
-    private void setupUserTestScript(StringBuffer expectedOutput, StringBuffer input) {
+    @Test
+    public void testGameThrowsExceptionWhenFewerThanTwoPlayers() throws IOException {
 
-        final String TRY_TO_ADD_ONE_PLAYER = "1";
-        final String TRY_TO_ADD_NINE_PLAYERS = "9";
-        final String TRY_TO_ADD_TWO_PLAYERS = "2";
+    }
 
-        expectedOutput.append(Game.HOW_MANY_PLAYERS);
-        input.append(line(TRY_TO_ADD_ONE_PLAYER));
-        expectedOutput.append(Game.INVALID_NUMBER_OF_PLAYERS);
-        expectedOutput.append(Game.HOW_MANY_PLAYERS);
-        input.append(line(TRY_TO_ADD_NINE_PLAYERS));
-        expectedOutput.append(Game.INVALID_NUMBER_OF_PLAYERS);
-        expectedOutput.append(Game.HOW_MANY_PLAYERS);
-        input.append(line(TRY_TO_ADD_TWO_PLAYERS));
-        expectedOutput.append(String.format(Game.SELECT_TOKEN, PLAYER_ONE));
-        expectedOutput.append("(B)oot, (R)ace car, (C)at, (D)og, Top (h)at, Battle(s)hip, (T)himble, (W)heelbarrow");
-        input.append(line("B"));
-        expectedOutput.append(String.format(Game.SELECT_TOKEN, PLAYER_TWO));
-        expectedOutput.append("(R)ace car, (C)at, (D)og, Top (h)at, Battle(s)hip, (T)himble, (W)heelbarrow");
-        input.append(line("x"));
-        expectedOutput.append(Game.INVALID_TOKEN_LETTER);
-        expectedOutput.append(String.format(Game.SELECT_TOKEN, PLAYER_TWO));
-        expectedOutput.append("(R)ace car, (C)at, (D)og, Top (h)at, Battle(s)hip, (T)himble, (W)heelbarrow");
-        input.append(line("h"));
-
+    @Test
+    public void testGameThrowsExceptionWhenMoreThanEightPlayers() throws IOException {
     }
 
     @Test
     public void testPlayersAreNotAlwaysInSameOrder() throws IOException {
-        int playersMatch = 0;
 
-        for (int counter = 0; counter < 100; counter++) {
-            createTwoPlayerGame(expectedOutput, input);
-            Game game = createGame();
-
-            if (playersMatch(game)) {
-                playersMatch++;
-            }
-
-        }
-        assertTrue(playersMatch >= 1 && playersMatch <= 99);
-        System.out.println(String.format("Number of matches: %d", playersMatch));
-
-    }
-
-    private void createTwoPlayerGame(StringBuffer expectedOutput, StringBuffer input) {
-        final String ADD_TWO_PLAYERS = "2";
-        input.append(line(ADD_TWO_PLAYERS));
-        input.append(line("B"));
-        input.append(line("h"));
-    }
-
-    private boolean playersMatch(Game game) {
-        Player player1 = new Player(new Token("Boot"));
-        Player player2 = new Player(new Token("Top hat"));
-        return (player1.equals(game.getPlayer(FIRST_PLAYER)) && player2.equals(game.getPlayer(SECOND_PLAYER)));
     }
 
     @Test
     public void testGameOfTwentyRounds() throws IOException {
-        final int NUMBER_OF_ROUNDS = 20;
-
-        setupTwentyRounds(expectedOutput, input);
-        Game game = createGame();
-        assertEquals(NUMBER_OF_ROUNDS, game.getNumberOfRounds());
-        assertEquals(NUMBER_OF_ROUNDS, game.getPlayer(FIRST_PLAYER).getNumberOfTurns());
-        assertEquals(NUMBER_OF_ROUNDS, game.getPlayer(SECOND_PLAYER).getNumberOfTurns());
     }
-
-    private void setupTwentyRounds(StringBuffer expectedOutput, StringBuffer input) {
-        createTwoPlayerGame(expectedOutput, input);
-    }
-
 }
