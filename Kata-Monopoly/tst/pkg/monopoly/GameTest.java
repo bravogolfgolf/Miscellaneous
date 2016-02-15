af7cd2a71f6db87b7f5f1749c7a9db40be087e80
bravogolfgolf <bggibson@hotmail.com>
2016-02-15 15:44:30 +0100

    realized testing through UI is bad move...going to refactor and remove UI code for now and focus on functionality

--- Kata-Monopoly/tst/pkg/monopoly/GameTest.java
+++ Kata-Monopoly/tst/pkg/monopoly/GameTest.java
@@ -96,7 +96,6 @@ public class GameTest {
             }
 
         }
-        assertEquals(expectedOutput.toString(), outputStream.toString());
         assertTrue(playersMatch >= 1 && playersMatch <= 99);
         System.out.println(String.format("Number of matches: %d", playersMatch));
 
@@ -104,13 +103,8 @@ public class GameTest {
 
     private void createTwoPlayerGame(StringBuffer expectedOutput, StringBuffer input) {
         final String ADD_TWO_PLAYERS = "2";
-        expectedOutput.append(Game.HOW_MANY_PLAYERS);
         input.append(line(ADD_TWO_PLAYERS));
-        expectedOutput.append(String.format(Game.SELECT_TOKEN, PLAYER_ONE));
-        expectedOutput.append("(B)oot, (R)ace car, (C)at, (D)og, Top (h)at, Battle(s)hip, (T)himble, (W)heelbarrow");
         input.append(line("B"));
-        expectedOutput.append(String.format(Game.SELECT_TOKEN, PLAYER_TWO));
-        expectedOutput.append("(R)ace car, (C)at, (D)og, Top (h)at, Battle(s)hip, (T)himble, (W)heelbarrow");
         input.append(line("h"));
     }
 
@@ -126,7 +120,6 @@ public class GameTest {
 
         setupTwentyRounds(expectedOutput, input);
         Game game = createGame();
-        assertEquals(expectedOutput.toString(), outputStream.toString());
         assertEquals(NUMBER_OF_ROUNDS, game.getNumberOfRounds());
         assertEquals(NUMBER_OF_ROUNDS, game.getPlayer(FIRST_PLAYER).getNumberOfTurns());
         assertEquals(NUMBER_OF_ROUNDS, game.getPlayer(SECOND_PLAYER).getNumberOfTurns());
