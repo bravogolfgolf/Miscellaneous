85a8c00271ab591c15d4c95d46c4c8d8910cdbb4
bravogolfgolf <bggibson@hotmail.com>
2016-02-17 17:34:27 +0100

    refactoring

--- Kata-Monopoly/tst/pkg/monopoly/PlayerTest.java
+++ Kata-Monopoly/tst/pkg/monopoly/PlayerTest.java
@@ -7,45 +7,45 @@ import static org.junit.Assert.assertEquals;
 
 public class PlayerTest {
 
-    private Player player1;
+    private Player player;
+    private DiceMock diceMock;
+    private Board board;
 
     @Before
     public void setup() {
-        player1 = new Player();
+        player = new Player();
+        diceMock = new DiceMock();
+        board = new Board();
     }
 
     @Test
     public void testCreatePlayer() {
         final Space EXPECTED_INITIAL_LOCATION = new Go("Go");
         final int EXPECTED_INITIAL_CASH_BALANCE = 1500;
-        assertEquals(EXPECTED_INITIAL_LOCATION, player1.getSpace());
-        assertEquals(EXPECTED_INITIAL_CASH_BALANCE, player1.getCashBalance());
+        assertEquals(EXPECTED_INITIAL_LOCATION, player.getSpace());
+        assertEquals(EXPECTED_INITIAL_CASH_BALANCE, player.getCashBalance());
     }
 
     @Test
     public void testMovesAndDoesNoWrap() {
-        DiceMock diceMock = new DiceMock();
-        Board board = new Board();
-        player1.setSpace(new Go("Go"));
-        player1.takeATurn(diceMock);
+        player.setSpace(board.getSpace(0));
+        player.takeATurn(diceMock);
         Space endingLocation = board.getSpace(2);
-        assertEquals(endingLocation, player1.getSpace());
+        assertEquals(endingLocation, player.getSpace());
     }
 
     @Test
     public void testMovesAndWraps() {
-        DiceMock diceMock = new DiceMock();
-        Board board = new Board();
-        player1.setSpace(board.getSpace(39));
-        player1.takeATurn(diceMock);
+        player.setSpace(board.getSpace(39));
+        player.takeATurn(diceMock);
         Space endingLocation = board.getSpace(1);
-        assertEquals(endingLocation, player1.getSpace());
+        assertEquals(endingLocation, player.getSpace());
     }
 
     @Test
     public void testIncreaseCashBalance(){
-        int expectedBalance = player1.getCashBalance() + 100;
-        player1.increaseCashBalanceBy(100);
-        assertEquals(expectedBalance,player1.getCashBalance());
+        int expectedBalance = player.getCashBalance() + 100;
+        player.increaseCashBalanceBy(100);
+        assertEquals(expectedBalance, player.getCashBalance());
     }
 }
