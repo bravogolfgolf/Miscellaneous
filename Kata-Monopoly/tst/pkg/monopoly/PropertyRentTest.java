355ddf1351382da19b3c1da69a22953bc45eb619
bravogolfgolf <bggibson@hotmail.com>
2016-02-23 00:22:33 +0100

    start here in morning

---
+++ Kata-Monopoly/tst/pkg/monopoly/PropertyRentTest.java
@@ -0,0 +1,13 @@
+package pkg.monopoly;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class PropertyRentTest {
+
+    @Test
+    public void testIfAllPropertiesInGroupAreOwnedBySamePlayer(){
+        assertTrue(false);
+    }
+}
