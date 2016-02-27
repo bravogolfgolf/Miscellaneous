d9eb9f66735fd3dd8c08015e05845857ca5ed610
bravogolfgolf <bggibson@hotmail.com>
2016-02-27 21:26:06 +0100

    Test for card order pass

--- Kata-Monopoly/src/pkg/monopoly/Game.java
+++ Kata-Monopoly/src/pkg/monopoly/Game.java
@@ -5,7 +5,6 @@ import java.util.*;
 
 public class Game {
 
-    private int numberOfCards;
 
     public Game(String localization) throws IOException {
         String spacesFileName = String.format("Spaces_%s.txt",localization);
@@ -14,33 +13,33 @@ public class Game {
             board.get(i).setNextSpace(board.get(i + 1));
         }
         board.get(board.size() - 1).setNextSpace(board.get(0));
-    }
 
-    public void randomizeCardOrder() {
-    }
-
-    public int getNumberOfCards() {
-        return numberOfCards;
-    }
-
-    public Card getCard(int index) {
-        return null;
+        String cardsFileName = String.format("Cards_%s.txt",localization);
+        cards = Card.load(cardsFileName);
     }
 
     public class InvalidPlayerCount extends Exception {
+
         public InvalidPlayerCount(String message) {
             super(message);
         }
+
     }
 
     public static final int MINIMUM_NUMBER_OF_PLAYERS = 2;
     public static final int MAXIMUM_NUMBER_OF_PLAYERS = 8;
+
     private List<Space> board;
+    private List<Card> cards;
     List<Player> players = new ArrayList<Player>();
     Dice dice = new Dice();
 
     public List<Space> getBoard() {
         return board;
+    }
+
+    public Card getCard(int index) {
+        return cards.get(index);
     }
 
     public void addPlayer(Player player) {
@@ -61,6 +60,14 @@ public class Game {
 
     public void randomizePlayerOrder() {
         Collections.shuffle(players);
+    }
+
+    public int getNumberOfCards() {
+        return cards.size();
+    }
+
+    public void randomizeCardOrder() {
+        Collections.shuffle(cards);
     }
 
     public void start() throws InvalidPlayerCount {
