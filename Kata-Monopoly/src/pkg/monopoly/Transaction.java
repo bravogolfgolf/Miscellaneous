8eefc3d5ab892be066b270583ef9e414f0ab240a
bravogolfgolf <bggibson@hotmail.com>
2016-03-03 14:07:28 +0100

    How to pay/collect from all other players based on community chest card drawn passes

--- Kata-Monopoly/src/pkg/monopoly/Transaction.java
+++ Kata-Monopoly/src/pkg/monopoly/Transaction.java
@@ -1,5 +1,8 @@
 package pkg.monopoly;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Transaction extends Card {
     private int amount = 0;
     private String recipient = "";
@@ -23,6 +26,25 @@ public class Transaction extends Card {
     public void action(Player player) throws GoToJail.GoToJailException {
         if (recipient.equals("Bank"))
             player.changeCashBalanceBy(amount);
-            // TODO figure out how to get all players still in game
+        if (recipient.equals("Players")) {
+            List<Player> otherPlayers = getAllOtherPlayersInGame(player);
+            int multipleAmount = amount * otherPlayers.size();
+            player.changeCashBalanceBy(multipleAmount);
+            for (Player otherPlayer : otherPlayers) {
+                otherPlayer.changeCashBalanceBy(-amount);
+            }
+        }
+    }
+
+    private List<Player> getAllOtherPlayersInGame(Player player) {
+        List<Player> otherPlayers = new ArrayList<Player>();
+        Player currentPlayer = player;
+        Player nextPlayer = currentPlayer.getNextPlayer();
+        while (!nextPlayer.equals(player)) {
+            otherPlayers.add(nextPlayer);
+            currentPlayer = nextPlayer;
+            nextPlayer = currentPlayer.getNextPlayer();
+        }
+        return otherPlayers;
     }
 }
