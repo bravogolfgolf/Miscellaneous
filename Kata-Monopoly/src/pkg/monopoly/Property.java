0e5160b80b198d3b38be6f6a8448526b19921fd8
bravogolfgolf <bggibson@hotmail.com>
2016-03-08 14:21:47 +0100

    rearranged methods

--- Kata-Monopoly/src/pkg/monopoly/Property.java
+++ Kata-Monopoly/src/pkg/monopoly/Property.java
@@ -53,125 +53,6 @@ abstract class Property extends Space {
         return (int) -(mortgageAmount() * 1.10);
     }
 
-    public void setOwner(Player owner) {
-        this.owner = owner;
-    }
-
-    public Player getOwner() {
-        return owner;
-    }
-
-    public List<Space> getAllPropertiesInGroup() {
-        List<Space> properties = new ArrayList<Space>();
-        properties.add(this);
-        String group = this.getGroup();
-        Space startingSpace = this;
-        Space currentSpace = this;
-        Space nextSpace = currentSpace.getNextSpace();
-        while (!nextSpace.equals(startingSpace)) {
-            if (nextSpace.getGroup().equals(group))
-                properties.add(nextSpace);
-            currentSpace = nextSpace;
-            nextSpace = currentSpace.getNextSpace();
-        }
-        return properties;
-    }
-
-    public boolean allPropertiesHaveSameOwner(List<Space> properties) {
-        Player thisOwner = this.getOwner();
-        for (int i = 1; i < properties.size(); i++) {
-            Property next = (Property) properties.get(i);
-            if (!next.getOwner().equals(thisOwner))
-                return false;
-        }
-        return true;
-    }
-
-    public int getCountOfPropertiesInGroupWithSameOwner(List<Space> properties) {
-        Player thisOwner = this.getOwner();
-        int ownerCount = 0;
-        for (Space space : properties) {
-            Property property = (Property) space;
-            if (property.getOwner().equals(thisOwner))
-                ownerCount++;
-        }
-        return ownerCount;
-    }
-
-    @Override
-    public void landOn(Player player, SourceOfMoveMultiplier sourceOfMoveMultiplier, OwnershipMultiplier ownershipMultiplier) {
-        if (playerIsNotOwner(player))
-            if (propertyIsUnowned())
-                // TODO Add ability to buy property or auction
-                buyProperty(player);
-            else if (propertyIsNotMortgaged()) {
-                if (ownershipMultiplier.isDefault())
-                    ownershipMultiplier = overwriteOwnershipMultiplier();
-                int rentOwed = calculateRentOwed(sourceOfMoveMultiplier, ownershipMultiplier);
-                payRent(player, rentOwed);
-            }
-    }
-
-    private OwnershipMultiplier overwriteOwnershipMultiplier() {
-        List<Space> propertiesInGroup = getAllPropertiesInGroup();
-        if (isRailroad()) {
-            int exponent = getCountOfPropertiesInGroupWithSameOwner(propertiesInGroup) - 1;
-            int ownershipMultiplier = (int) Math.pow(2, exponent);
-            return new OwnershipMultiplier(ownershipMultiplier);
-        }
-
-        if (isUtility())
-            if (allPropertiesHaveSameOwner(propertiesInGroup))
-                return new OwnershipMultiplier(10);
-            else
-                return new OwnershipMultiplier(4);
-
-        if (allPropertiesHaveSameOwner(propertiesInGroup) && isUnimproved())
-            return new OwnershipMultiplier(2);
-
-        return new OwnershipMultiplier();
-    }
-
-    private boolean isUnimproved() {
-        return numberOfImprovements == 0;
-    }
-
-    public void addImprovements() {
-        numberOfImprovements++;
-    }
-
-    public void removeImprovements() {
-        numberOfImprovements--;
-    }
-
-    public int getImprovements() {
-        return numberOfImprovements;
-    }
-
-    protected abstract int calculateRentOwed(SourceOfMoveMultiplier sourceOfMoveMultiplier, OwnershipMultiplier ownershipMultiplier);
-
-    private boolean playerIsNotOwner(Player player) {
-        return !player.equals(this.getOwner());
-    }
-
-    private boolean propertyIsUnowned() {
-        return owner.isBank();
-    }
-
-    private boolean propertyIsNotMortgaged() {
-        return !isMortgaged;
-    }
-
-    private void buyProperty(Player player) {
-        player.transaction(-price, "Purchase");
-        this.owner = player;
-    }
-
-    private void payRent(Player player, int rentOwed) {
-        player.transaction(-rentOwed, "Cash");
-        this.owner.transaction(rentOwed, "Cash");
-    }
-
     public void mortgagedBy(Player player) {
         if (mortgageConditionsAreMeet(player)) {
             player.transaction(mortgageAmount(), "Mortgage");
@@ -196,5 +77,124 @@ abstract class Property extends Space {
 
     private boolean unMortgageConditionsAreMeet(Player player) {
         return isMortgaged() && propertyIsOwnedByPlayer(player);
+    }
+
+    public void setOwner(Player owner) {
+        this.owner = owner;
+    }
+
+    public Player getOwner() {
+        return owner;
+    }
+
+    public void addImprovements() {
+        numberOfImprovements++;
+    }
+
+    public void removeImprovements() {
+        numberOfImprovements--;
+    }
+
+    public int getImprovements() {
+        return numberOfImprovements;
+    }
+
+    @Override
+    public void landOn(Player player, SourceOfMoveMultiplier sourceOfMoveMultiplier, OwnershipMultiplier ownershipMultiplier) {
+        if (playerIsNotOwner(player))
+            if (propertyIsUnowned())
+                // TODO Add ability to buy property or auction
+                buyProperty(player);
+            else if (propertyIsNotMortgaged()) {
+                if (ownershipMultiplier.isDefault())
+                    ownershipMultiplier = overwriteOwnershipMultiplier();
+                int rentOwed = calculateRentOwed(sourceOfMoveMultiplier, ownershipMultiplier);
+                payRent(player, rentOwed);
+            }
+    }
+
+    private boolean playerIsNotOwner(Player player) {
+        return !player.equals(this.getOwner());
+    }
+
+    private boolean propertyIsUnowned() {
+        return owner.isBank();
+    }
+
+    private void buyProperty(Player player) {
+        player.transaction(-price, "Purchase");
+        this.owner = player;
+    }
+
+    private boolean propertyIsNotMortgaged() {
+        return !isMortgaged;
+    }
+
+    private OwnershipMultiplier overwriteOwnershipMultiplier() {
+        List<Space> propertiesInGroup = getAllPropertiesInGroup();
+        if (isRailroad()) {
+            int exponent = getCountOfPropertiesInGroupWithSameOwner(propertiesInGroup) - 1;
+            int ownershipMultiplier = (int) Math.pow(2, exponent);
+            return new OwnershipMultiplier(ownershipMultiplier);
+        }
+
+        if (isUtility())
+            if (allPropertiesHaveSameOwner(propertiesInGroup))
+                return new OwnershipMultiplier(10);
+            else
+                return new OwnershipMultiplier(4);
+
+        if (allPropertiesHaveSameOwner(propertiesInGroup) && isUnimproved())
+            return new OwnershipMultiplier(2);
+
+        return new OwnershipMultiplier();
+    }
+
+    public List<Space> getAllPropertiesInGroup() {
+        List<Space> properties = new ArrayList<Space>();
+        properties.add(this);
+        String group = this.getGroup();
+        Space startingSpace = this;
+        Space currentSpace = this;
+        Space nextSpace = currentSpace.getNextSpace();
+        while (!nextSpace.equals(startingSpace)) {
+            if (nextSpace.getGroup().equals(group))
+                properties.add(nextSpace);
+            currentSpace = nextSpace;
+            nextSpace = currentSpace.getNextSpace();
+        }
+        return properties;
+    }
+
+    public int getCountOfPropertiesInGroupWithSameOwner(List<Space> properties) {
+        Player thisOwner = this.getOwner();
+        int ownerCount = 0;
+        for (Space space : properties) {
+            Property property = (Property) space;
+            if (property.getOwner().equals(thisOwner))
+                ownerCount++;
+        }
+        return ownerCount;
+    }
+
+    public boolean allPropertiesHaveSameOwner(List<Space> properties) {
+        Player thisOwner = this.getOwner();
+        for (int i = 1; i < properties.size(); i++) {
+            Property next = (Property) properties.get(i);
+            if (!next.getOwner().equals(thisOwner))
+                return false;
+        }
+        return true;
+    }
+
+    private boolean isUnimproved() {
+        return numberOfImprovements == 0;
+    }
+
+    protected abstract int calculateRentOwed(SourceOfMoveMultiplier sourceOfMoveMultiplier, OwnershipMultiplier ownershipMultiplier);
+
+    private void payRent(Player player, int rentOwed) {
+        player.transaction(-rentOwed, "Cash");
+        this.owner.transaction(rentOwed, "Cash");
     }
 }
