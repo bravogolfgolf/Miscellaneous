9aef59c7df3c7f9eae28c5ac4bed5877937cf77f
bravogolfgolf <bggibson@hotmail.com>
2016-03-18 19:22:31 +0100

    Clean up misc repo

--- Kata-Sort/src/sort/Sorter.java
+++ Kata-Sort/src/sort/Sorter.java
@@ -3,9 +3,9 @@ package sort;
 import java.util.Arrays;
 import java.util.List;
 
-public class Sorter {
+class Sorter {
 
-	public static List<Integer> sort(Integer...ints) {
+	static List<Integer> sort(Integer...ints) {
 		List<Integer> asList = Arrays.asList(ints);
 		if(asList.size() > 1){
 			for (int i = 1; i < asList.size(); i++) {
